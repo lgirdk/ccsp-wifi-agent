e3be4d6a6547b1c5849a2297d1a15d094da86b98
Andre McCurdy <armccurdy@gmail.com>
2020-04-11 20:42:21 -0700

    make _Band_Switch() conditional on _ENABLE_BAND_STEERING_
    All the references to _Band_Switch() are conditional on
    _ENABLE_BAND_STEERING_, so make the actual function conditional too.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/TR-181/sbapi/cosa_wifi_apis.c
+++ source/TR-181/sbapi/cosa_wifi_apis.c
@@ -16302,6 +16302,7 @@ CosaDmlWiFi_SetBandSteeringOptions(PCOSA_DML_WIFI_BANDSTEERING_OPTION  pBandStee
 	return ANSC_STATUS_SUCCESS;
 }
 
+#if defined(_ENABLE_BAND_STEERING_)
 #if defined (_PLATFORM_RASPBERRYPI_) || defined(_PLATFORM_TURRIS_)
 void *_Band_Switch( void *arg)
 {
@@ -16333,6 +16334,7 @@ void _wifi_eventCapture(void){
 	system("iw event -f > /tmp/event_count.txt");
 	//pthread_exit(NULL);
 }
+#endif
 #endif
 
 ANSC_STATUS 
