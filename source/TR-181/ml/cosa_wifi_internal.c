e7a5b85cb3740aeb6fed8bafde4ec06dc64eadc0
Hamza Ben Zekri <hbenzekri@libertyglobal.com>
2020-09-09 13:15:05 +0200

    Add support for X_LGI-COM_Bandsteering datamodel ( RDKB-4676 )
    Port the Arris Mv2 changes for X_ARRIS_COM_Bandsteering and adapt them according to the OFW requirements.

--- source/TR-181/ml/cosa_wifi_internal.c
+++ source/TR-181/ml/cosa_wifi_internal.c
@@ -970,6 +970,8 @@ CosaWifiInitialize
     if (pWifiAp != NULL)
         CosaWifiRegGetMacFiltInfo(pWifiAp);
 
+    CosaLgiWifiInitialize((ANSC_HANDLE)pMyObject);
+
 	//CosaWifiRegGetATMInfo((ANSC_HANDLE)pMyObject);
 	pATM = (PCOSA_DML_WIFI_ATM)AnscAllocateMemory(sizeof(COSA_DML_WIFI_ATM));
     if ( NULL != pATM )	{		
@@ -1188,7 +1190,8 @@ CosaWifiReInitialize
 		//	pMyObject->pBandSteering->pBSSettings[1].InstanceNumber = 2;
 		//	CosaDmlWiFi_GetBandSteeringSettings( 1, pMyObject->pBandSteering->pBSSettings+1 );
 		//}		
-	
+	CosaLgiWifiReInitialize((ANSC_HANDLE)pMyObject);
+
     EXIT:
     return returnStatus;
 }
@@ -1325,7 +1328,7 @@ CosaWifiReInitializeRadioAndAp
 		//	CosaDmlWiFi_GetBandSteeringSettings( 1, pMyObject->pBandSteering->pBSSettings+1 );
 		//}		
 	}
-	
+	CosaLgiWifiReInitializeRadioAndAp((ANSC_HANDLE)pMyObject);
     return returnStatus;
 }
 /**********************************************************************
@@ -1442,6 +1445,7 @@ CosaWifiRemove
     /* Remove self */
     AnscFreeMemory((ANSC_HANDLE)pMyObject);
 
+    CosaLgiWifiRemove((ANSC_HANDLE)pMyObject);
 	return returnStatus;
 }
 
