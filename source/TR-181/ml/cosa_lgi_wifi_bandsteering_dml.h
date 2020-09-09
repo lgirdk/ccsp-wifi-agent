0fd96227f667575a8e9b57c0bffce2efba7036dd
Hamza Ben Zekri <hbenzekri@libertyglobal.com>
2020-09-09 13:15:05 +0200

    Add support for X_LGI-COM_Bandsteering datamodel ( RDKB-4676 )
    Port Mv2 changes for X_ARRIS_COM_Bandsteering and adapt them according to the OFW requirements.

---
+++ source/TR-181/ml/cosa_lgi_wifi_bandsteering_dml.h
@@ -0,0 +1,56 @@
+/*********************************************************************
+ * Copyright 2017-2019 ARRIS Enterprises, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **********************************************************************/
+
+#ifndef  _COSA_LGI_WIFI_BANDSTEERING_DML_H_
+#define  _COSA_LGI_WIFI_BANDSTEERING_DML_H_
+
+typedef struct
+_COSA_DML_BANDSTEERING_SSID_CAPABLE5G_ENTRY
+{
+    char    MacAddress[18];
+    char    EntryTime[32];
+}
+COSA_DML_BANDSTEERING_SSID_CAPABLE5G_ENTRY, *PCOSA_DML_BANDSTEERING_SSID_CAPABLE5G_ENTRY;
+
+typedef struct
+_COSA_DML_BANDSTEERING_SSID_BLACKLIST24G_ENTRY
+{
+    char    MacAddress[18];
+    char    EntryTime[32];
+    UINT    TimeRemaining;
+}
+COSA_DML_BANDSTEERING_SSID_BLACKLIST24G_ENTRY, *PCOSA_DML_BANDSTEERING_SSID_BLACKLIST24G_ENTRY;
+
+
+typedef  struct
+_COSA_DML_BANDSTEERING_SSID
+{
+    INT     ifIndex;
+    BOOL    Enable;
+    BOOL    Active;
+    BOOL    ClearCapable5G;
+    BOOL    ClearBlacklist24G;
+    INT     RSSIThreshold;
+    UINT    DeltaThreshold;
+    UINT    BlacklistTimeout;
+    ULONG   pCapable5GEntryCount;
+    PCOSA_DML_BANDSTEERING_SSID_CAPABLE5G_ENTRY pCapable5G;
+    ULONG   pBlacklist24GEntryCount;
+    PCOSA_DML_BANDSTEERING_SSID_BLACKLIST24G_ENTRY pBlacklist24G;
+}
+COSA_DML_BANDSTEERING_SSID,  *PCOSA_DML_BANDSTEERING_SSID;
+
+#endif
