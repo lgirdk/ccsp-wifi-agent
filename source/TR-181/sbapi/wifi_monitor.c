43182293dd36002c8503f598494caef371d32392
Andre McCurdy <armccurdy@gmail.com>
2020-09-09 23:57:43 -0700

    fix various missing or unnecessary function return values in wifi_monitor.c
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/TR-181/sbapi/wifi_monitor.c
+++ source/TR-181/sbapi/wifi_monitor.c
@@ -3057,7 +3057,6 @@ void GetActiveMsmtPlanID(unsigned int *pPlanID)
     {
         memcpy(pPlanID, g_active_msmt.active_msmt.PlanId, PLAN_ID_LENGTH);
     }
-    return NULL;
 }
 
 /*********************************************************************************/
@@ -3080,7 +3079,6 @@ void GetActiveMsmtStepSrcMac(mac_address_t pStepSrcMac)
     {
         memcpy(pStepSrcMac, g_active_msmt.curStepData.SrcMac, sizeof(mac_address_t));
     }
-    return NULL;
 }
 
 /*********************************************************************************/
@@ -3103,7 +3101,6 @@ void GetActiveMsmtStepDestMac(mac_address_t pStepDstMac)
     {
         memcpy(pStepDstMac, g_active_msmt.curStepData.DestMac, sizeof(mac_address_t));
     }
-    return NULL;
 }
 
 
@@ -4000,7 +3997,7 @@ void *WiFiBlastClient(void* data)
         frameCountSample = NULL;
     }
     wifi_dbg_print(1, "%s : %d exiting the function\n",__func__,__LINE__);
-    return;
+    return NULL;
 }
 /*********************************************************************************/
 /*                                                                               */
