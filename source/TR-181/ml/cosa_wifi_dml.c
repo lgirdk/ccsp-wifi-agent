d8e469bc33011c3293725efabeff94af7e0bb0e3
Mustafa Karadzha <mkaradzha@libertyglobal.com>
2020-09-23 13:38:49 +0200

    add support for Device.WiFi.Radio.{i}.X_LGI-COM_EnhancedACS.ExcludeDFS ( RDKB-4728 )
    - Add support for "Device.WiFi.Radio.{i}.X_LGI-COM_EnhancedACS.ExcludeDFS"
    - Ensure there is no conflict between "ExcludeDFS" and "X_COMCAST_COM_DFSEnable"
    - Reload the wifi on Country code changes

--- source/TR-181/ml/cosa_wifi_dml.c
+++ source/TR-181/ml/cosa_wifi_dml.c
@@ -4258,6 +4258,12 @@ Radio_SetParamBoolValue
             return  TRUE;
         }
 
+        // Only allow disabling DFS if ExcludeDFS is enabled.
+        if ((pWifiRadioFull->Cfg.EnhancedACS.ExcludeDFS == FALSE) && (bValue == FALSE))
+        {
+            return FALSE;
+        }
+
         /* save update to backup */
         pWifiRadioFull->Cfg.X_COMCAST_COM_DFSEnable = bValue;
         pWifiRadio->bRadioChanged = TRUE;
@@ -6496,6 +6502,10 @@ EnhancedACS_GetParamBoolValue
          *pBool = pWifiRadioEnhancedACS->DFSMoveBack;
          return TRUE;
     }
+    if (strcmp(ParamName, "ExcludeDFS") == 0) {
+         *pBool = pWifiRadioEnhancedACS->ExcludeDFS;
+         return TRUE;
+    }
     return FALSE;
 }
 
@@ -6519,6 +6529,16 @@ EnhancedACS_SetParamBoolValue
     }
     if (strcmp(ParamName, "DFSMoveBack") == 0) {
         pWifiRadioEnhancedACS->DFSMoveBack = bValue;
+        return TRUE;
+    }
+    if (strcmp(ParamName, "ExcludeDFS") == 0) {
+        if (pWifiRadioEnhancedACS->ExcludeDFS != bValue) {
+            // Only allow NOT excluding DFS if DFS is enabled.
+            if ((pWifiRadioFull->Cfg.X_COMCAST_COM_DFSEnable == FALSE) && (bValue == FALSE)) {
+                return FALSE;
+            }
+            pWifiRadioEnhancedACS->ExcludeDFS = bValue;
+        }
         return TRUE;
     }
     return FALSE;
