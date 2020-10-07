db797525a78c5c4a41f20b68da109cd0e515d970
Hamza Ben Zekri <hbenzekri@libertyglobal.com>
2020-10-07 10:42:38 +0200

    Adapt ActiveTimeout feature to Mv1 Atom ( RDKB-4761 )

--- source/TR-181/ml/cosa_wifi_dml.c
+++ source/TR-181/ml/cosa_wifi_dml.c
@@ -10062,7 +10062,12 @@ AccessPoint_SetParamStringValue
             int    iGnIndex24 = 15, iGnIndex50 = 16;
 
             //remove the current entry from crontab, if any
+#if defined(_LG_MV1_CELENO_)
+            system("rpcclient 192.168.254.253 'sed -i \'/Device.WiFi.SSID./d\' /var/spool/cron/crontabs/root'");
+            system("rpcclient 192.168.254.253 'sed -i \'/Device.WiFi.Radio./d\' /var/spool/cron/crontabs/root'");
+#else
             system("sed -i '/Device.WiFi.SSID./d' /var/spool/cron/crontabs/root");
+#endif
 
             if (pString[0] != '\0')
             {
@@ -10073,15 +10078,25 @@ AccessPoint_SetParamStringValue
                 sscanf(pString,"%d/%d/%d-%d:%d", &iDay, &iMonth, &iYear, &iHour, &iMin);
 
                 //prepare the crontab entry
+#if defined(_LG_MV1_CELENO_)
+                snprintf (strCronCmd, sizeof(strCronCmd), "rpcclient 192.168.254.253 'echo \"%d %d %d %d * dmcli eRT setvalue Device.WiFi.SSID.%d.Enable bool false; dmcli eRT setvalue Device.WiFi.Radio.1.X_CISCO_COM_ApplySetting bool true\" >> /var/spool/cron/crontabs/root'",
+                    iMin, iHour, iDay, iMonth, iGnIndex24);
+#else
                 sprintf (strCronCmd, "echo '%d %d %d %d * dmcli eRT setvalue Device.WiFi.SSID.%d.Enable bool false' >> /var/spool/cron/crontabs/root",
                     iMin, iHour, iDay, iMonth, iGnIndex24);
+#endif
 
                 //add the crontab entry
                 system(strCronCmd);
 
                 //prepare the next crontab entry
+#if defined(_LG_MV1_CELENO_)
+                snprintf (strCronCmd, sizeof(strCronCmd), "rpcclient 192.168.254.253 'echo \"%d %d %d %d * dmcli eRT setvalue Device.WiFi.SSID.%d.Enable bool false; dmcli eRT setvalue Device.WiFi.Radio.2.X_CISCO_COM_ApplySetting bool true\" >> /var/spool/cron/crontabs/root'",
+                    iMin, iHour, iDay, iMonth, iGnIndex50);
+#else
                 sprintf (strCronCmd, "echo '%d %d %d %d * dmcli eRT setvalue Device.WiFi.SSID.%d.Enable bool false' >> /var/spool/cron/crontabs/root",
                     iMin, iHour, iDay, iMonth, iGnIndex50);
+#endif
 
                 //add the next crontab entry
                 system(strCronCmd);
