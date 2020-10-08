b487495cc2f8da091f389762229fb3d0bfcde9e3
jyoti <jjyoti@libertyglobal.com>
2020-10-08 18:27:41 +0200

    agent side changes related to WiFi Channel Stats ( RDKB-4678 )
    Ported WiFi Channel Stats changes.
    
    Signed-off-by: jyoti <jjyoti@libertyglobal.com>

--- source/TR-181/sbapi/cosa_wifi_apis.h
+++ source/TR-181/sbapi/cosa_wifi_apis.h
@@ -67,7 +67,7 @@
 #include "cosa_apis.h"
 #include "ccsp_base_api.h"
 #include "wifi_hal.h"
-
+#include "cosa_lgi_wifi_radio_stats_dml.h" //LGI ADD
 
 //#include "secure_wrapper.h"
 
@@ -581,6 +581,11 @@ _COSA_DML_WIFI_RADIO_STATS
     ULONG                           StatisticsStartTime;
 	ULONG							LastSampling;
 	COSA_DML_WIFI_RADIO_STATS_RSL	RslInfo;
+        // LGI ADD BEGIN
+        ULONG                           ChannelCount;
+        ULONG                           ChannelStatsUpdateTime;
+        PCOSA_DML_CHANNEL_STATS         ChannelStats;
+        // LGI ADD END
 }_struct_pack_;
 
 typedef  struct _COSA_DML_WIFI_RADIO_STATS COSA_DML_WIFI_RADIO_STATS, *PCOSA_DML_WIFI_RADIO_STATS;
