810117a098c0b57df0394b60b38d3671f48e3a5e
jyoti <jjyoti@libertyglobal.com>
2020-10-08 18:27:41 +0200

    agent side changes related to WiFi Channel Stats ( RDKB-4678 )
    Ported WiFi Channel Stats changes.
    
    Signed-off-by: jyoti <jjyoti@libertyglobal.com>

--- source/TR-181/ml/cosa_wifi_internal.c
+++ source/TR-181/ml/cosa_wifi_internal.c
@@ -621,6 +621,10 @@ CosaWifiInitialize
                     pWifiRadio->Radio.Cfg.InstanceNumber = uIndex + 1;
                 }				
             }
+            pWifiRadio->Stats.ChannelStats = NULL;
+            pWifiRadio->Stats.ChannelCount = 0;
+            pWifiRadio->Stats.ChannelStatsUpdateTime = 0;
+
          }
     }
     
