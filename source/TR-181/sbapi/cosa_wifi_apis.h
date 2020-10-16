5ef87731b0581fc147f9beebc2ec39ba88e2c31b
Karthik Shanbhag <kshanbhag@libertyglobal.com>
2020-10-16 13:14:46 +0200

    update forbidden SSID's as per OFW requirements ( RDKB-4764 )
    [Description]
    Prevent forbidden SSID being set
    
    Signed-off-by: Karthik Shanbhag(kshanbhag@libertyglobal.com)
    
    Support to update forbidden WiFi SSID names ( RDKB-4837 )
    
    [Description]
    New data model added as per the requirements MVXREQ-916 and MVXREQ-1189
    Device.WiFi.X_LGI-COM_ReservedSSIDNames (string (512))
    
    Signed-off-by: Karthik Shanbhag(kshanbhag@libertyglobal.com)
    
    fix wrong Wifi informtion displayed on Screen when factory reset done via UI ( OFW-150 )
    
    [Description]
    Below data models are fetching wrong values from PSM.
    - Device.WiFi.SSID.$i.X_COMCAST-COM_DefaultSSID
    - Device.WiFi.AccessPoint.1.Security.X_COMCAST-COM_DefaultKeyPassphrase
    
    [Solution]
    Implement HAL apis to get right values instead of PSM values.
    get the default SSId and password from HAL.
    
    Signed-off-by: Sidda(sihiregoudar@libertyglobal.com)
    
    fix forbidden private WiFi SSIDs accepted ( OFW-1509 )
    
    [Description]
    Forbidden private WiFi SSIDs accepted in MV2+
    Issue was due to code was in #define specific to MV1, but this code should be common.
    Also requirement changed after the initial DM changes were made to this.
    As per new requirement, Device.WiFi.X_LGI-COM_ReservedSSIDNames by default empty string.
    And this DM should contain only addition reserved names added by Operator.
    
    Signed-off-by: Karthik Shanbhag(kshanbhag@libertyglobal.com)
    
    ignore spaces, dashes and underscores when checked for reserved SSID names
    
    Normalise SSID names before comparing against reserved SSID names
    lists. ie variations such as e.g.:
    
      "Horizon Wi-free"    <-- lower case "free"
      " Horizon Wi-Free"   <-- leading space
      "_Horizon Wi-Free"   <-- leading underscore
    
    would all be considered a match for the reserved SSID name
    "Horizon Wi-Free".
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    verify values set to X_LGI-COM_ReservedSSIDNames are single SSIDs
    
    Commas are used as separators for the lists of reserved SSIDs,
    therefore we need to enforce a rule that no reserved SSID can contain
    a comma.
    
    This also protects against attempts to set a list of SSIDs in one SPV
    (which is not supported - reserved SSIDs need to be added one at a
    time).
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/TR-181/sbapi/cosa_wifi_apis.h
+++ source/TR-181/sbapi/cosa_wifi_apis.h
@@ -1436,6 +1436,19 @@ CosaDmlWiFi_GetInterworkingInternetAvailable
     );
 
 ANSC_STATUS
+CosaDmlWiFi_SetWiFiReservedSSIDNames
+    (
+        ANSC_HANDLE phContext,
+        char *ReservedName
+    );
+
+int isReservedSSID
+    (
+        char *ReservedNames,
+        char *ssid
+    );
+
+ANSC_STATUS
 CosaDmlWiFi_GetPreferPrivateData
     (
        BOOL *value
