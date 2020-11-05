65dc1c80daf3a080275e0a6764a01cf44bf60625
Gokulakrishnan Ramalingam (Infosys) <gramalingam.contractor@libertyglobal.com>
2020-11-05 11:37:24 +0100

    add support for RADIUS accounting server ip and secret ( RDKB-4846 )
    Implement the data model of ServerIPAddr, SecondaryServerIPAddr,
    ServerPort, SecondaryServerPort, Secret, SecondarySecret. Details:
    
      Device.WiFi.AccessPoint.i.Accounting.ServerIPAddr
      Device.WiFi.AccessPoint.i.Accounting.SecondaryServerIPAddr
      Device.WiFi.AccessPoint.i.Accounting.ServerPort
      Device.WiFi.AccessPoint.i.Accounting.SecondaryServerPort
      Device.WiFi.AccessPoint.i.Accounting.Secret
      Device.WiFi.AccessPoint.i.Accounting.SecondarySecret
    
    TESTING PERFORMED:
    Uses the dmcli commands and ACS to set and get the parameters
    
    Signed-off-by: Gokulakrishnan Ramalingam <gramalingam.contractor@libertyglobal.com>

--- source/TR-181/ml/cosa_wifi_internal.c
+++ source/TR-181/ml/cosa_wifi_internal.c
@@ -823,11 +823,13 @@ CosaWifiInitialize
 #if !defined(_COSA_INTEL_USG_ATOM_) && !defined(_COSA_BCM_MIPS_) && !defined(_COSA_BCM_ARM_) && !defined(_PLATFORM_TURRIS_)
         CosaDmlWiFiApGetEntry((ANSC_HANDLE)pMyObject->hPoamWiFiDm, pWifiSsid->SSID.Cfg.SSID, &pWifiAp->AP);   
         CosaDmlWiFiApSecGetEntry((ANSC_HANDLE)pMyObject->hPoamWiFiDm, pWifiSsid->SSID.Cfg.SSID, &pWifiAp->SEC);
+        CosaDmlWiFiApAcctGetEntry((ANSC_HANDLE)pMyObject->hPoamWiFiDm, pWifiSsid->SSID.StaticInfo.Name, &pWifiAp->ACCT);
         CosaDmlWiFiApWpsGetEntry((ANSC_HANDLE)pMyObject->hPoamWiFiDm, pWifiSsid->SSID.Cfg.SSID, &pWifiAp->WPS);
         CosaDmlWiFiApMfGetCfg((ANSC_HANDLE)pMyObject->hPoamWiFiDm, pWifiSsid->SSID.Cfg.SSID, &pWifiAp->MF);
 #else
         CosaDmlWiFiApGetEntry((ANSC_HANDLE)pMyObject->hPoamWiFiDm, pWifiSsid->SSID.StaticInfo.Name, &pWifiAp->AP);   
         CosaDmlWiFiApSecGetEntry((ANSC_HANDLE)pMyObject->hPoamWiFiDm, pWifiSsid->SSID.StaticInfo.Name, &pWifiAp->SEC);
+        CosaDmlWiFiApAcctGetEntry((ANSC_HANDLE)pMyObject->hPoamWiFiDm, pWifiSsid->SSID.StaticInfo.Name, &pWifiAp->ACCT);
         CosaDmlWiFiApWpsGetEntry((ANSC_HANDLE)pMyObject->hPoamWiFiDm, pWifiSsid->SSID.StaticInfo.Name, &pWifiAp->WPS);
         CosaDmlWiFiApMfGetCfg((ANSC_HANDLE)pMyObject->hPoamWiFiDm, pWifiSsid->SSID.StaticInfo.Name, &pWifiAp->MF);
 #endif
@@ -1348,6 +1350,7 @@ CosaWifiReInitialize
 #else
                     CosaDmlWiFiApGetEntry((ANSC_HANDLE)pMyObject->hPoamWiFiDm, pWifiSsid->SSID.StaticInfo.Name, &pWifiAp->AP);   
                     CosaDmlWiFiApSecGetEntry((ANSC_HANDLE)pMyObject->hPoamWiFiDm, pWifiSsid->SSID.StaticInfo.Name, &pWifiAp->SEC);
+                    CosaDmlWiFiApAcctGetEntry((ANSC_HANDLE)pMyObject->hPoamWiFiDm, pWifiSsid->SSID.StaticInfo.Name, &pWifiAp->ACCT);
                     CosaDmlWiFiApWpsGetEntry((ANSC_HANDLE)pMyObject->hPoamWiFiDm, pWifiSsid->SSID.StaticInfo.Name, &pWifiAp->WPS);
 #endif
 
@@ -1555,6 +1558,7 @@ CosaWifiReInitializeRadioAndAp
 #else
 		CosaDmlWiFiApGetEntry((ANSC_HANDLE)pMyObject->hPoamWiFiDm, pWifiSsid->SSID.StaticInfo.Name, &pWifiAp->AP);   
 		CosaDmlWiFiApSecGetEntry((ANSC_HANDLE)pMyObject->hPoamWiFiDm, pWifiSsid->SSID.StaticInfo.Name, &pWifiAp->SEC);
+		CosaDmlWiFiApAcctGetEntry((ANSC_HANDLE)pMyObject->hPoamWiFiDm, pWifiSsid->SSID.StaticInfo.Name, &pWifiAp->ACCT);
 		CosaDmlWiFiApWpsGetEntry((ANSC_HANDLE)pMyObject->hPoamWiFiDm, pWifiSsid->SSID.StaticInfo.Name, &pWifiAp->WPS);
 #endif	
 	}
