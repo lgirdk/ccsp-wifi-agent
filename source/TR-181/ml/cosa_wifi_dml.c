7fa4e255030bddfb1ab0aacafc3d49e3d5c47560
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2020-11-06 13:40:06 +0100

    Only Device.WiFi.X_CISCO_COM_FactoryReset bool true would trigger WiFi module reset.(RDKB-4894)
    DESCRIPTION:-
    
       Only Device.WiFi.X_CISCO_COM_FactoryReset bool true would trigger WiFi module reset.
       Device.WiFi.X_CISCO_COM_FactoryReset bool false would not trigger WiFi module reset.
    
    TESTING PERFORMED:
    
    Verfied that Device.WiFi.X_CISCO_COM_FactoryReset works as expected.

--- source/TR-181/ml/cosa_wifi_dml.c
+++ source/TR-181/ml/cosa_wifi_dml.c
@@ -897,8 +897,10 @@ WiFi_SetParamBoolValue
     PCOSA_DATAMODEL_WIFI            pMyObject     = (PCOSA_DATAMODEL_WIFI)g_pCosaBEManager->hWifi;
 
     if(AnscEqualString(ParamName, "X_CISCO_COM_FactoryReset", TRUE))
-    {
-        CosaDmlWiFi_FactoryReset();
+    {   if (bValue)
+        {
+            CosaDmlWiFi_FactoryReset();
+        }
         return TRUE;
     }
 
