7744741d8dac6a8e044fe2c9df57359ea7449cb8
Gokulakrishnan Ramalingam (Infosys) <gramalingam.contractor@libertyglobal.com>
2020-11-11 16:18:57 +0100

    support specifying RADIUS authentication server UDP port numbers ( RDKB-4847 )
    FIX DESCRIPTION:
    Implement the data model of RadiusServerIPAddr, SecondaryRadiusServerIPAddr,
    RadiusServerPort, SecondaryRadiusServerPort, RadiusSecret, SecondaryRadiusSecret.
    
    Details:
      Device.WiFi.AccessPoint.{i}.Security.RadiusServerIPAddr
      Device.WiFi.AccessPoint.{i}.Security.RadiusServerPort
      Device.WiFi.AccessPoint.{i}.Security.RadiusSecret
      Device.WiFi.AccessPoint.{i}.Security.SecondaryRadiusServerIPAddr
      Device.WiFi.AccessPoint.{i}.Security.SecondaryRadiusServerPort
      Device.WiFi.AccessPoint.{i}.Security.SecondaryRadiusSecret
    
    TESTING PERFORMED:
    Uses the dmcli commands set and get the parameters
    
    Signed-off-by: Gokulakrishnan Ramalingam gramalingam.contractor@libertyglobal.com

--- source/TR-181/sbapi/cosa_wifi_apis.c
+++ source/TR-181/sbapi/cosa_wifi_apis.c
@@ -13603,6 +13603,7 @@ wifiDbgPrintf("%s\n",__FUNCTION__);
 		strcmp(pCfg->RadiusSecret, pStoredCfg->RadiusSecret) !=0) {
 		CcspWifiTrace(("RDK_LOG_WARN,\n%s calling wifi_setApSecurityRadiusServer  \n",__FUNCTION__));        
 		wifi_setApSecurityRadiusServer(wlanIndex, pCfg->RadiusServerIPAddr, pCfg->RadiusServerPort, pCfg->RadiusSecret);
+		enable_reset_radio_flag(wlanIndex);
     }
 
 	if ( strcmp(pCfg->SecondaryRadiusServerIPAddr, pStoredCfg->SecondaryRadiusServerIPAddr) !=0 || 
@@ -13610,6 +13611,7 @@ wifiDbgPrintf("%s\n",__FUNCTION__);
 		strcmp(pCfg->SecondaryRadiusSecret, pStoredCfg->SecondaryRadiusSecret) !=0) {
 		CcspWifiTrace(("RDK_LOG_WARN,\n%s calling wifi_setApSecurityRadiusServer  \n",__FUNCTION__));
 		wifi_setApSecuritySecondaryRadiusServer(wlanIndex, pCfg->SecondaryRadiusServerIPAddr, pCfg->SecondaryRadiusServerPort, pCfg->SecondaryRadiusSecret);
+		enable_reset_radio_flag(wlanIndex);
 	}
 
 	if ( strcmp(pCfg->MFPConfig, pStoredCfg->MFPConfig) !=0 ) {
