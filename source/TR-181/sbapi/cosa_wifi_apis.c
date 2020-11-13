d438a542d71b140fff6c95b2b0c65cfad3b9e58a
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2020-11-13 11:27:38 +0100

    Removing outdated way of updating wmm param(RDKB-4970)
    DESCRIPTION:-
    
    CosaDmlWiFiCheckWmmParams is the outdated way of udating wwm params from PSM defaults.
    
    WMMEnable, UAPSDEnable and WmmNoAck now read from hal cfg.
    
    TESTING PERFORMED:-
    
    Verified the default value of wwm params after reboot.

--- source/TR-181/sbapi/cosa_wifi_apis.c
+++ source/TR-181/sbapi/cosa_wifi_apis.c
@@ -8805,7 +8805,7 @@ printf("%s: Reset FactoryReset to 0 \n",__FUNCTION__);
 
         //zqiu: do not merge
 //        CosaDmlWiFiCheckSecurityParams();
-        CosaDmlWiFiCheckWmmParams();
+        //CosaDmlWiFiCheckWmmParams();
 
         //>>zqiu
 		// Fill Cache
