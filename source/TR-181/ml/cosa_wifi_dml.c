af30ea9d6e3db6038862880808040be6ece15137
Sandipan Sinha <ssinha.contractor@libertyglobal.com>
2020-11-23 21:31:54 +0100

    fix Device.WiFi.AccessPoint.{i}.WMMEnable can't be set ( RDKB-4971 )
    Fix for Device.WiFi.AccessPoint.{i}.WMMEnable can't be set with error popup
    and after that is resolved if set False then UAPSD should also become False.
    
    BUG DESCRIPTION:
    - WMM cannot be set to false when UAPSD is Enabled.
    - UAPSD should also turn False when WMM is set to False.
    
    TESTING PERFORMED:
    
    - Set Device.WiFi.AccessPoint.{i}.WMMEnable to False successfully.
    - Make sure Device.WiFi.AccessPoint.{i}.UAPSDEnable also reflects False.
    
    Signed-off-by: Sandipan Sinha <ssinha.contractor@libertyglobal.com>

--- source/TR-181/ml/cosa_wifi_dml.c
+++ source/TR-181/ml/cosa_wifi_dml.c
@@ -8040,6 +8040,10 @@ AccessPoint_SetParamBoolValue
         
         /* save update to backup */
         pWifiAp->AP.Cfg.WMMEnable = bValue;
+        /*  when disable WMM, make sure UAPSD is disabled as well */
+        if(bValue == FALSE) {
+             pWifiAp->AP.Cfg.UAPSDEnable = bValue;
+        }
         pWifiAp->bApChanged = TRUE;
         return TRUE;
     }
@@ -8049,6 +8053,10 @@ AccessPoint_SetParamBoolValue
         if ( pWifiAp->AP.Cfg.UAPSDEnable == bValue )
         {
             return  TRUE;
+        }
+        if ( TRUE == bValue && FALSE == pWifiAp->AP.Cfg.WMMEnable )
+        {
+            return FALSE;
         }
         /* save update to backup */
         pWifiAp->AP.Cfg.UAPSDEnable = bValue;
@@ -8713,12 +8721,14 @@ AccessPoint_Validate
     }
 
     /* UAPSD can not be enabled when WMM is disabled */
+#if 0
     if((FALSE == pWifiAp->AP.Cfg.WMMEnable) && (TRUE == pWifiAp->AP.Cfg.UAPSDEnable))
     {
        AnscCopyString(pReturnParamName, "UAPSDEnable");
        *puLength = sizeof("UAPSDEnable");
         goto EXIT;
     }
+#endif
              /* ManagementFramePowerControl Parameter values higher than 0 shall be converted to value of 0 and Parameter values lower than -20 shall be converted to value of -20 */
     if(pWifiAp->AP.Cfg.ManagementFramePowerControl > 0)
     {
