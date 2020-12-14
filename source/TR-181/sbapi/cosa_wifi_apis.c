1178b227e60f4479e970e0ed5dc11cc5d1cdc594
Gokulakrishnan Ramalingam (Infosys) <gramalingam.contractor@libertyglobal.com>
2020-12-14 18:00:01 +0100

    support for restarting Radius Relay ( RDKB-4988 )
    TESTING PERFORMED:-
     - Verified Radius authendication via CM interface only with IPV4 mode.
     - Verified that radius relay is up and running during boot up.
     - Verified that radiusrelay-restart when radius setting changed and it is working as expected.
    
    Signed-off-by: Gokulakrishnan Ramalingam <gramalingam.contractor@libertyglobal.com>
    
    integrate radius relay interface with radius relay ( RDKB-5024 )
    
    DESCRIPTION:
    
    - This change is made so that radius relay process restarts after
      radius interface is changed.
    
    Signed-off-by: Sandipan Sinha <ssinha.contractor@libertyglobal.com>

--- source/TR-181/sbapi/cosa_wifi_apis.c
+++ source/TR-181/sbapi/cosa_wifi_apis.c
@@ -149,6 +149,7 @@ void CosaDmlWiFiPsmDelInterworkingEntry();
 #endif
 BOOL gRadioRestartRequest[3]={FALSE,FALSE,FALSE};
 BOOL g_newXH5Gpass=FALSE;
+BOOL gRestartRadiusRelay = FALSE;
 
 #if defined(_ENABLE_BAND_STEERING_)
 ANSC_STATUS CosaDmlWiFi_GetBandSteeringLog_2();
@@ -3660,6 +3661,11 @@ setRadiusTransportInterfaceintoPSM(int val)
 			{
 				AnscTraceError(("%scfg set failed for Radius Transport Interface\n", __FUNCTION__));
 				retVal = ANSC_STATUS_FAILURE;
+			}
+			else
+			{
+				//Triggering radius relay restart after radius interface changed
+				gRestartRadiusRelay = TRUE;
 			}
 		}
 		else
@@ -10762,6 +10768,15 @@ fprintf(stderr, "----# %s %d gRadioRestartRequest=%d %d \n", __func__, __LINE__,
                     v_secure_system("/usr/bin/sysevent set wifi_RadioChannel 'RDK|%lu|%lu'", pCfg->InstanceNumber-1, pCfg->Channel);
 		}
 #endif
+        if(gRestartRadiusRelay)
+        {
+            gRestartRadiusRelay = FALSE;
+#if defined(INTEL_MV1)
+            CcspWifiTrace(("RDK_LOG_INFO,WIFI %s : restart Radius Relay \n",__FUNCTION__));
+            //Trigerring radius relay restart after radius setting changed
+            system("rpcclient 192.168.254.253 'sysevent set radiusrelay-restart'");
+#endif
+        }
     }
     else
     {
@@ -13067,6 +13082,7 @@ CosaDmlSetApRadiusSettings
     }
     else
     {
+        gRestartRadiusRelay = TRUE;
         enable_reset_radio_flag(wlanIndex);
     }
 
@@ -13630,6 +13646,7 @@ wifiDbgPrintf("%s\n",__FUNCTION__);
         wifi_setApBeaconType(wlanIndex, securityType);
 		CcspWifiTrace(("RDK_LOG_WARN,\n%s calling setBasicAuthenticationMode ssid : %s authmode : %s \n",__FUNCTION__,pSsid,authMode));
         wifi_setApBasicAuthenticationMode(wlanIndex, authMode);
+        gRestartRadiusRelay = TRUE;
 #if defined(ENABLE_FEATURE_MESHWIFI)
         // Notify Mesh components of an AP config change
         MeshNotifySecurityChange(wlanIndex, pCfg);
@@ -13679,6 +13696,7 @@ wifiDbgPrintf("%s\n",__FUNCTION__);
                 }
 
         wifi_setApSecurityPreSharedKey(wlanIndex, pCfg->PreSharedKey);
+        gRestartRadiusRelay = TRUE;
         }
     }
 
@@ -13744,6 +13762,7 @@ wifiDbgPrintf("%s\n",__FUNCTION__);
 #endif
 		CcspWifiTrace(("RDK_LOG_WARN,\n RDKB_WIFI_CONFIG_CHANGED :%s Encryption method changed ,calling setWpaEncryptionMode Index : %d mode : %s \n",__FUNCTION__,wlanIndex,method));
 		wifi_setApWpaEncryptionMode(wlanIndex, method);
+        gRestartRadiusRelay = TRUE;
 		enable_reset_radio_flag(wlanIndex);
     } 
 
@@ -13751,6 +13770,7 @@ wifiDbgPrintf("%s\n",__FUNCTION__);
     if ( pCfg->RekeyingInterval != pStoredCfg->RekeyingInterval) {
 		CcspWifiTrace(("RDK_LOG_WARN,\n%s calling setWpaRekeyInterval  \n",__FUNCTION__));
         wifi_setApSecurityWpaRekeyInterval(wlanIndex,  pCfg->RekeyingInterval);
+        gRestartRadiusRelay = TRUE;
     }
 #endif
 
@@ -13759,6 +13779,7 @@ wifiDbgPrintf("%s\n",__FUNCTION__);
 		strcmp(pCfg->RadiusSecret, pStoredCfg->RadiusSecret) !=0) {
 		CcspWifiTrace(("RDK_LOG_WARN,\n%s calling wifi_setApSecurityRadiusServer  \n",__FUNCTION__));        
 		wifi_setApSecurityRadiusServer(wlanIndex, pCfg->RadiusServerIPAddr, pCfg->RadiusServerPort, pCfg->RadiusSecret);
+		gRestartRadiusRelay = TRUE;
 		enable_reset_radio_flag(wlanIndex);
     }
 
@@ -13767,6 +13788,7 @@ wifiDbgPrintf("%s\n",__FUNCTION__);
 		strcmp(pCfg->SecondaryRadiusSecret, pStoredCfg->SecondaryRadiusSecret) !=0) {
 		CcspWifiTrace(("RDK_LOG_WARN,\n%s calling wifi_setApSecurityRadiusServer  \n",__FUNCTION__));
 		wifi_setApSecuritySecondaryRadiusServer(wlanIndex, pCfg->SecondaryRadiusServerIPAddr, pCfg->SecondaryRadiusServerPort, pCfg->SecondaryRadiusSecret);
+		gRestartRadiusRelay = TRUE;
 		enable_reset_radio_flag(wlanIndex);
 	}
 
