470cf4060b4dda0d266f9ace72b2ce7459edfba0
Sandipan Sinha <ssinha.contractor@libertyglobal.com>
2020-12-21 17:42:15 +0100

    associate Radius Relay with Radius Transport Interface ( RDKB-5019 )
    DESCRIPTION:
    
    - Made further changes to save/set the RadiusTransport Interface into cfg.
    - Made little change to the enumeration( eCM , eRouter ) for ease of saving value into cfg.
    
    TESTING PERFORMED:
    
    - cfg get/set for "AP_RADIUS_GO_HFC" successful.
    - Default value reflects as "eCM" after Factory Reset.
    - Performed successful getv and setv via dmcli for Device.WiFi.X_LGI-COM_Radius.TransportInterface
    - Made sure after set call the value gets reflected in nvram/bbhm_cur_cfg.xml because that's what
      will decide the interface for radius communication.
    - psmcli calls are successful.
    - Changed the value to "eRouter" and rebooted the device and made sure it is persistent over reboot.
    
    Signed-off-by: Sandipan Sinha <ssinha.contractor@libertyglobal.com>

--- source/TR-181/sbapi/cosa_wifi_apis.c
+++ source/TR-181/sbapi/cosa_wifi_apis.c
@@ -3797,11 +3797,46 @@ else {
 return ANSC_STATUS_SUCCESS;
 }
 
-void setRadiusTransportInterfaceintoPSM(int val)
+ANSC_STATUS
+setRadiusTransportInterfaceintoPSM(int val)
 {
-        char strValue[10];
-        snprintf(strValue,sizeof(strValue),"%d",val);
-        PSM_Set_Record_Value2(bus_handle,g_Subsystem, TransportInterface, ccsp_string, strValue);
+	char *storedValue  = NULL;
+	char strValue[10];
+	int retVal;
+	retVal = PSM_Get_Record_Value2( bus_handle, g_Subsystem, TransportInterface, NULL, &storedValue );
+	if  (retVal == CCSP_SUCCESS)
+	{
+		if(val != storedValue)
+		{
+			snprintf(strValue,sizeof(strValue),"%d",val);
+			retVal = PSM_Set_Record_Value2(bus_handle,g_Subsystem, TransportInterface, ccsp_string, strValue);
+			if  (retVal != CCSP_SUCCESS)
+			{
+				AnscTraceError(("%spsm set failed for Radius Transport Interface\n", __FUNCTION__));
+				retVal = ANSC_STATUS_FAILURE;
+			}
+			else
+			{
+				retVal = ANSC_STATUS_SUCCESS;
+			}
+			retVal = wifi_setApRadiusTransportInterface(val);
+			if (retVal != ANSC_STATUS_SUCCESS)
+			{
+				AnscTraceError(("%scfg set failed for Radius Transport Interface\n", __FUNCTION__));
+				retVal = ANSC_STATUS_FAILURE;
+			}
+		}
+		else
+		{
+			retVal = ANSC_STATUS_SUCCESS;
+		}
+	}
+	else
+	{
+		retVal = ANSC_STATUS_FAILURE;
+		AnscTraceError(("%spsm get failed for Radius Transport Interface\n", __FUNCTION__));
+	}
+return retVal;
 }
 
 
