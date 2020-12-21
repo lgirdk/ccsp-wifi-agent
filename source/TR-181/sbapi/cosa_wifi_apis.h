8319f21f3231e0d5fe77a063de28aad9d8445988
Sandipan Sinha <ssinha.contractor@libertyglobal.com>
2020-12-21 17:42:15 +0100

    associate Radius Relay with Radius Transport Interface ( RDKB-5019 )
    DESCRIPTION:
    
    - Made further changes to save/set the RadiusTransport Interface into cfg.
    - Made little change to the enumeration( eCM , eRouter ) for ease of saving value into cfg.
    
    TESTING PERFORMED:
    
    - cfg get/set for "AP_RADIUS_GO_HFC" successful.
    - Default value reflects as "eCM" after Factory Reset.
    - Performed successful getv and setv via dmcli for Device.WiFi.X_LGI-COM_Radius.TransportInterface
    - Made sure after set call the value gets reflected in nvram/bbhm_cur_cfg.xml because that's what
      will decide the interface for radius communication.
    - psmcli calls are successful.
    - Changed the value to "eRouter" and rebooted the device and made sure it is persistent over reboot.
    
    Signed-off-by: Sandipan Sinha <ssinha.contractor@libertyglobal.com>

--- source/TR-181/sbapi/cosa_wifi_apis.h
+++ source/TR-181/sbapi/cosa_wifi_apis.h
@@ -1495,7 +1495,7 @@ void CosaDmlWiFi_UpdateMfCfg(void);
 
 ANSC_STATUS getRadiusTransportInterface(int *radiusInterface);
 
-void setRadiusTransportInterfaceintoPSM(int val);
+ANSC_STATUS setRadiusTransportInterfaceintoPSM(int val);
 
 
 ANSC_STATUS
