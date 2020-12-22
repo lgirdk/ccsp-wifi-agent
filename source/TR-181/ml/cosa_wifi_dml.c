997cb17b95364b3dd820ce3a3d528cfb3cb92c48
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2020-12-22 08:18:44 +0100

    support for specifying the re-authentication behavior for a RADIUS-authenticated Community WiFi client ( RDKB-4981 )
    DESCRIPTION:-
    
    The RADIUS client in the Gateway MUST support configuration of a Reauthentication Interval via the datamodel parameter
    Device.WiFi.AccessPoint.{i}.Security.X_COMCAST-COM_RadiusSettings.ReAuthInterval
    
    Device.WiFi.AccessPoint.{i}.Security.X_LGI-COM_Radius.ReAuthInterval (From tr069)
    
    TESTING PERFORMED:-
    
    1. Make DUT up after factory reset.
    2. Verified the default value for community WiFi SSID's are 3600.
    3. Verified dmcli SPV and GPV on the parameter is working properly (Verified from PSM entries)

--- source/TR-181/ml/cosa_wifi_dml.c
+++ source/TR-181/ml/cosa_wifi_dml.c
@@ -20912,6 +20912,13 @@ RadiusSettings_GetParamIntValue
         return TRUE;
     }
 
+    if (strcmp(ParamName, "ReAuthInterval") == 0)
+    {
+        /* collect value */
+        *pInt = pWifiAp->AP.RadiusSetting.iReAuthInterval;
+        return TRUE;
+    }
+
     if (strcmp(ParamName, "MaxAuthenticationAttempts") == 0)
     {
         /* collect value */
@@ -21038,6 +21045,14 @@ RadiusSettings_SetParamIntValue
         pWifiAp->AP.RadiusSetting.iPMKCacheInterval = iValue;
         return TRUE;
     }
+
+    if (strcmp(ParamName, "ReAuthInterval") == 0)
+    {
+        /* save update to backup */
+        pWifiAp->AP.RadiusSetting.iReAuthInterval = iValue;
+        return TRUE;
+    }
+
     if (strcmp(ParamName, "MaxAuthenticationAttempts") == 0)
     {
         /* save update to backup */
