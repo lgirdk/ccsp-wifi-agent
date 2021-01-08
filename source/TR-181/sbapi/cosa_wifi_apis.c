ad0612bf8113e1bc5dc611fe422ba20324a5593d
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2021-01-08 16:02:09 +0100

    support for specifying the re-authentication behavior for a RADIUS-authenticated Community WiFi client ( RDKB-4981 )
    DESCRIPTION:-
    
    The RADIUS client in the Gateway MUST support configuration of a Reauthentication Interval via the datamodel parameter
    Device.WiFi.AccessPoint.{i}.Security.X_COMCAST-COM_RadiusSettings.ReAuthInterval
    
    Device.WiFi.AccessPoint.{i}.Security.X_LGI-COM_Radius.ReAuthInterval (From tr069)
    
    TESTING PERFORMED:-
    
    1. Make DUT up after factory reset.
    2. Confgiured radius server and and radius reauth interval by following SPV's
    
    dmcli eRT setv Device.WiFi.SSID.{i}.SSID string "test5Gpriv"
    dmcli eRT setv Device.WiFi.AccessPoint.{i}.Security.RadiusServerIPAddr string xx.xx.xx.xx
    dmcli eRT setv Device.WiFi.AccessPoint.{i}.Security.RadiusSecret string xxxxxxx
    dmcli eRT setv Device.WiFi.AccessPoint.{i}.Security.ModeEnabled string WPA2-Enterprise
    dmcli eRT setv Device.WiFi.AccessPoint.{i}.Security.RadiusServerPort uint 1812
    dmcli eRT setv Device.WiFi.AccessPoint.{i}.Security.X_COMCAST-COM_RadiusSettings.ReAuthInterval int 60
    dmcli eRT setv Device.WiFi.Radio.{i}.X_CISCO_COM_ApplySetting bool true
    
    3. Verified that the change is reflecting at /tmp/hostapd-wdev0ap{i}.conf ( at wpa_ptk_rekey by default in MNG) for 5G.
    
    4. Did packet capture at connected client and accesspoint interface for both 5G and 2.4 G.
    
    5. Verified EAPOL 4 way handshake repeats in every 60 sec.

--- source/TR-181/sbapi/cosa_wifi_apis.c
+++ source/TR-181/sbapi/cosa_wifi_apis.c
@@ -7754,6 +7754,7 @@ void RemoveInvalidMacFilterListFromPsm()
     }
 }
 
+#if 0
 static ANSC_STATUS getRadiusAuthInterval (int index, UINT *radiusInterval)
 {
     char recName[256];
@@ -7775,6 +7776,7 @@ static ANSC_STATUS getRadiusAuthInterval (int index, UINT *radiusInterval)
 
     return ANSC_STATUS_SUCCESS;
 }
+#endif
 
 static ANSC_STATUS setRadiusAuthIntervalintoPSM (int index, UINT val)
 {
@@ -13307,10 +13309,10 @@ CosaDmlGetApRadiusSettings
         pRadiusSetting->iQuietPeriodAfterFailedAuthentication              = radSettings.QuietPeriodAfterFailedAuthentication;
     }
 
-    int AuthInterval;
-    if( getRadiusAuthInterval( wlanIndex , &AuthInterval) != ANSC_STATUS_SUCCESS )
+    unsigned int AuthInterval;
+    if( wifi_getApRadiusReAuthInterval(wlanIndex, &AuthInterval) )
     {
-        CcspWifiTrace(("RDK_LOG_ERR, RadiusReAuthInterval get from PSm failed\n"));
+        CcspWifiTrace(("RDK_LOG_ERR, RadiusReAuthInterval get failed\n"));
     }
     else
     {
@@ -13357,9 +13359,13 @@ CosaDmlSetApRadiusSettings
     }
 
     CcspWifiTrace(("RDK_LOG_WARN,\n%s calling setRadiusAuthIntervalintoPSM \n",__FUNCTION__));
-    if (setRadiusAuthIntervalintoPSM(wlanIndex, pRadiusSetting->iReAuthInterval) != ANSC_STATUS_SUCCESS)
+    if (setRadiusAuthIntervalintoPSM(wlanIndex, pRadiusSetting->iReAuthInterval) != ANSC_STATUS_SUCCESS || wifi_setApRadiusReAuthInterval(wlanIndex,pRadiusSetting->iReAuthInterval) != 0)
     {
         CcspWifiTrace(("RDK_LOG_ERR,\n%s RadiusReAuthInterval failed to set \n",__FUNCTION__));
+    }
+    else
+    {
+        enable_reset_radio_flag(wlanIndex);
     }
 
 	radSettings.RadiusServerRetries  	=	pRadiusSetting->iRadiusServerRetries;
