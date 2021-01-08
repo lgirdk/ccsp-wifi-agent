b523a3cd42de4396cc2855ca5fdef2532bf0344b
Anu Chacko <achacko.contractor@libertyglobal.com>
2021-01-08 14:26:38 +0530

    disable PreferPrivate feature from RDKB which is similar to softblock ( RDKB-5051 )
    [Description]:
    - RDKB provides prefer private feature to allow softblock with the help of mac filtering the devices connected to private network.
    - This is similar to softblock functionality in MNG but not in line with CBN MV1 or CS MV2 current softblock feature.
    - Current MNG softblock feature allows softblock with help of softblocking from Celeno driver and softblocked clients can be cleared using the DML.
    
    [Test Description]:
    - After DUT is online, GPV of dmcli eRT getv Device.WiFi.X_RDKCENTRAL-COM_PreferPrivate. Value should be false.
    - There should not be any crash/issues when devices connected to WiFi SSIDs - private & hotspot

--- source/TR-181/sbapi/cosa_wifi_apis.c
+++ source/TR-181/sbapi/cosa_wifi_apis.c
@@ -7808,7 +7808,10 @@ printf("%s: Reset FactoryReset to 0 \n",__FUNCTION__);
 #endif
 	CosaDmlWiFi_startHealthMonitorThread();
 
+#if 0
     CosaDmlWiFiCheckPreferPrivateFeature(&(pMyObject->bPreferPrivateEnabled));
+#endif
+
 #if defined (FEATURE_SUPPORT_RADIUSGREYLIST)
     CosaDmlWiFiCheckEnableRadiusGreylist(&(pMyObject->bEnableRadiusGreyList));
 #endif
@@ -8377,6 +8380,7 @@ CosaDmlWiFi_SetPreferPrivatePsmData(BOOL value)
         return ANSC_STATUS_FAILURE;
     }
 
+#if 0
 #if !defined(_PLATFORM_RASPBERRYPI_) && !defined(_PLATFORM_TURRIS_)
    if(value == TRUE)
    {
@@ -8407,6 +8411,7 @@ CosaDmlWiFi_SetPreferPrivatePsmData(BOOL value)
     CosaDmlWiFi_UpdateMfCfg();
 #else
 	wifi_setPreferPrivateConnection(value);
+#endif
 #endif
     return ANSC_STATUS_SUCCESS;
 }
@@ -18518,6 +18523,7 @@ INT CosaDmlWiFi_AssociatedDevice_callback(INT apIndex, wifi_associated_dev_t *as
 		if(associated_dev->cli_Active == 1) 
 		{
 			Wifi_Hosts_Sync_Func(NULL,(apIndex+1), associated_dev, 0, 0);		
+#if 0
 			if ( ANSC_STATUS_SUCCESS == CosaDmlWiFi_GetPreferPrivateData(&bEnabled) )
 			{
 				if (bEnabled == TRUE)
@@ -18525,6 +18531,7 @@ INT CosaDmlWiFi_AssociatedDevice_callback(INT apIndex, wifi_associated_dev_t *as
 					Hotspot_Macfilter_sync(mac);
 				}
 			}
+#endif
 		}
 		else 				
 		{
