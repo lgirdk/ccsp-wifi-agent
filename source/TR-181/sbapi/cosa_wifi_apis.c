2576166aa05db3c668ea4ff661d2c0063361dca7
Gokulakrishnan Ramalingam (Infosys) <gramalingam.contractor@libertyglobal.com>
2021-01-12 18:17:56 +0100

    add support for Radius Accounting functionality ( RDKB-4989 )
    [DESCRIPTION]
    Make Radius Accounting functionality works
    
    [TESTING PERFORMED]
    Tested in both 2.4GHz and 5GHz
    
    1. Configure Radius server
    2. Configure Accounting Server
    3. Connect a wifi client in private SSID
    4. Clients should be able to connect successfully
    5. See the packet capture and verify the radius authentication and accounting request and accounting response.
    
    1. Configure Radius server
    2. Configure Accounting Server
    3. Connect a wifi client in hotspot SSID
    4. Clients should be able to connect successfully
    5. See the packet capture and verify the radius authentication and accounting request and accounting response.
    
    Signed-off-by: Gokulakrishnan Ramalingam gramalingam.contractor@libertyglobal.com

--- source/TR-181/sbapi/cosa_wifi_apis.c
+++ source/TR-181/sbapi/cosa_wifi_apis.c
@@ -18959,6 +18959,8 @@ CosaDmlWiFiApAcctSetCfg
             CcspWifiTrace(("RDK_LOG_ERROR,WIFI %s : could not set RADUIS account security server\n",__FUNCTION__));
             return ANSC_STATUS_FAILURE;
         }
+        gRestartRadiusRelay = TRUE;
+        enable_reset_radio_flag(wlanIndex);
     }
 
     if (strcmp(pCfg->SecondaryAcctServerIPAddr, pStoredCfg->SecondaryAcctServerIPAddr) !=0 ||
@@ -18970,6 +18972,8 @@ CosaDmlWiFiApAcctSetCfg
             CcspWifiTrace(("RDK_LOG_ERROR,WIFI %s : could not set RADUIS account security secondary server\n",__FUNCTION__));
             return ANSC_STATUS_FAILURE;
         }
+        gRestartRadiusRelay = TRUE;
+        enable_reset_radio_flag(wlanIndex);
     }
 
     if ( pCfg->InterimInterval != pStoredCfg->InterimInterval )
