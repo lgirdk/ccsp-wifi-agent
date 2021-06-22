c118be52f39bbc20fb6b41dd26485b625da63a0b
Andre McCurdy <armccurdy@gmail.com>
2021-06-22 00:58:38 -0700

    drop support for obsolete _COSA_DRG_CNS_ and _COSA_DRG_TPG_ platforms
    These are legacy Cisco platforms which are not supported or buildable
    in RDKB. They predate the introduction of the RDKB HAL APIs so the
    code isn't generally useful to keep around, even as a reference.
    
      for file in $(find . -type f -name '*.[ch]') ; do unifdef -U_COSA_DRG_TPG_ -U_COSA_DRG_CNS_ -o $file $file ; done
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source-pc/TR-181/ml/plugin_main.c
+++ source-pc/TR-181/ml/plugin_main.c
@@ -297,14 +297,6 @@ COSA_Async_Init
             pMoca->Remove(pMoca);
             g_pCosaBEManager->hMoCA = (ANSC_HANDLE)CosaMoCACreate();
         }
-#elif defined(_COSA_DRG_CNS_)
-        PCOSA_DATAMODEL_MOCA pMoca = g_pCosaBEManager->hMoCA;
-        pMoca->Remove(pMoca);
-        g_pCosaBEManager->hMoCA = (ANSC_HANDLE)CosaMoCACreate();
-#elif defined(_COSA_DRG_TPG_)
-        PCOSA_DATAMODEL_WIFI pWifi = g_pCosaBEManager->hWifi;
-        pWifi->Remove(pWifi);
-        g_pCosaBEManager->hWifi = (ANSC_HANDLE)CosaWifiCreate();
 #endif
 
     }
@@ -339,14 +331,6 @@ COSA_IsObjSupported
 
 #endif
 
-#if (defined(_COSA_DRG_CNS_))
-
-    if(AnscEqualString(pObjName, "Device.DNS.Client.", TRUE))
-    {
-        return FALSE;
-    }        
-
-#endif
 
     return TRUE;
 }
