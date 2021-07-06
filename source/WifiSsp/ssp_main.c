ad54e1a06df036dd412e83d62b054e72484bfd76
jyoti <jjyoti@libertyglobal.com>
2021-07-06 14:52:04 +0200

    add missing Radio ApplySettings in X_LGI-COM_ActiveTimeout in crontabs ( OFW-1470 )
    [Mv2+] Fix guest network status is shown as Enabled in ACS even though guest network is down.
    
    Radio ApplySettings was missing for X_LGI-COM_ActiveTimeout in crontabs .
    
    Signed-off-by: jyoti <jjyoti@libertyglobal.com>

--- source/WifiSsp/ssp_main.c
+++ source/WifiSsp/ssp_main.c
@@ -596,7 +596,7 @@ int main(int argc, char* argv[])
     system("rpcclient2 'sysevent set radiusrelay-start'");
 #endif
 
-    char*                           pParamNames[]      = {"Device.WiFi.AccessPoint.15.X_LGI-COM_ActiveTimeout"};
+    char*                           pParamNames[]      = {"Device.WiFi.AccessPoint.7.X_LGI-COM_ActiveTimeout"};
     parameterValStruct_t**          ppReturnVal        = NULL;
     parameterInfoStruct_t**         ppReturnValNames   = NULL;
     parameterAttributeStruct_t**    ppReturnvalAttr    = NULL;
@@ -623,6 +623,7 @@ int main(int argc, char* argv[])
         system("rpcclient2 'sed -i \'/Device.WiFi.Radio./d\' /var/spool/cron/crontabs/root'");
 #else
         system("sed -i '/Device.WiFi.SSID./d' /var/spool/cron/crontabs/root");
+        system("sed -i '/Device.WiFi.Radio./d' /var/spool/cron/crontabs/root");
 #endif
 
         //setup the crontab entry
@@ -631,7 +632,7 @@ int main(int argc, char* argv[])
         snprintf (strCronCmd, sizeof(strCronCmd), "rpcclient2 'echo \"%d %d %d %d * dmcli eRT setvalue Device.WiFi.SSID.%d.Enable bool false; dmcli eRT setvalue Device.WiFi.Radio.1.X_CISCO_COM_ApplySetting bool true\" >> /var/spool/cron/crontabs/root'",
             iMin, iHour, iDay, iMonth, iGnIndex24);
 #else
-        sprintf (strCronCmd, "echo '%d %d %d %d * dmcli eRT setvalue Device.WiFi.SSID.%d.Enable bool false' >> /var/spool/cron/crontabs/root",
+        sprintf (strCronCmd, "echo '%d %d %d %d * dmcli eRT setvalue Device.WiFi.SSID.%d.Enable bool false; dmcli eRT setvalue Device.WiFi.Radio.1.X_CISCO_COM_ApplySetting bool true' >> /var/spool/cron/crontabs/root",
             iMin, iHour, iDay, iMonth, iGnIndex24);
 #endif
 
@@ -643,7 +644,7 @@ int main(int argc, char* argv[])
         snprintf (strCronCmd, sizeof(strCronCmd), "rpcclient2 'echo \"%d %d %d %d * dmcli eRT setvalue Device.WiFi.SSID.%d.Enable bool false; dmcli eRT setvalue Device.WiFi.Radio.2.X_CISCO_COM_ApplySetting bool true\" >> /var/spool/cron/crontabs/root'",
             iMin, iHour, iDay, iMonth, iGnIndex50);
 #else
-        sprintf (strCronCmd, "echo '%d %d %d %d * dmcli eRT setvalue Device.WiFi.SSID.%d.Enable bool false' >> /var/spool/cron/crontabs/root",
+        sprintf (strCronCmd, "echo '%d %d %d %d * dmcli eRT setvalue Device.WiFi.SSID.%d.Enable bool false; dmcli eRT setvalue Device.WiFi.Radio.2.X_CISCO_COM_ApplySetting bool true' >> /var/spool/cron/crontabs/root",
             iMin, iHour, iDay, iMonth, iGnIndex50);
 #endif
 
