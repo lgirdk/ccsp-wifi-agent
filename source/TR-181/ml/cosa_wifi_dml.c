8b939f1e0314ccef5ae32d0d1b1aec219355c7f0
jyoti <jjyoti@libertyglobal.com>
2021-07-06 14:52:04 +0200

    add missing Radio ApplySettings in X_LGI-COM_ActiveTimeout in crontabs ( OFW-1470 )
    [Mv2+] Fix guest network status is shown as Enabled in ACS even though guest network is down.
    
    Radio ApplySettings was missing for X_LGI-COM_ActiveTimeout in crontabs .
    
    Signed-off-by: jyoti <jjyoti@libertyglobal.com>

--- source/TR-181/ml/cosa_wifi_dml.c
+++ source/TR-181/ml/cosa_wifi_dml.c
@@ -11219,6 +11219,7 @@ AccessPoint_SetParamStringValue
             system("rpcclient2 'sed -i \'/Device.WiFi.Radio./d\' /var/spool/cron/crontabs/root'");
 #else
             system("sed -i '/Device.WiFi.SSID./d' /var/spool/cron/crontabs/root");
+            system("sed -i '/Device.WiFi.Radio./d' /var/spool/cron/crontabs/root");
 #endif
 
             if (pString[0] != '\0')
@@ -11234,7 +11235,7 @@ AccessPoint_SetParamStringValue
                 snprintf (strCronCmd, sizeof(strCronCmd), "rpcclient2 'echo \"%d %d %d %d * dmcli eRT setvalue Device.WiFi.SSID.%d.Enable bool false; dmcli eRT setvalue Device.WiFi.Radio.1.X_CISCO_COM_ApplySetting bool true\" >> /var/spool/cron/crontabs/root'",
                     iMin, iHour, iDay, iMonth, iGnIndex24);
 #else
-                sprintf (strCronCmd, "echo '%d %d %d %d * dmcli eRT setvalue Device.WiFi.SSID.%d.Enable bool false' >> /var/spool/cron/crontabs/root",
+                sprintf (strCronCmd, "echo '%d %d %d %d * dmcli eRT setvalue Device.WiFi.SSID.%d.Enable bool false; dmcli eRT setvalue Device.WiFi.Radio.1.X_CISCO_COM_ApplySetting bool true' >> /var/spool/cron/crontabs/root",
                     iMin, iHour, iDay, iMonth, iGnIndex24);
 #endif
 
@@ -11246,7 +11247,7 @@ AccessPoint_SetParamStringValue
                 snprintf (strCronCmd, sizeof(strCronCmd), "rpcclient2 'echo \"%d %d %d %d * dmcli eRT setvalue Device.WiFi.SSID.%d.Enable bool false; dmcli eRT setvalue Device.WiFi.Radio.2.X_CISCO_COM_ApplySetting bool true\" >> /var/spool/cron/crontabs/root'",
                     iMin, iHour, iDay, iMonth, iGnIndex50);
 #else
-                sprintf (strCronCmd, "echo '%d %d %d %d * dmcli eRT setvalue Device.WiFi.SSID.%d.Enable bool false' >> /var/spool/cron/crontabs/root",
+                sprintf (strCronCmd, "echo '%d %d %d %d * dmcli eRT setvalue Device.WiFi.SSID.%d.Enable bool false; dmcli eRT setvalue Device.WiFi.Radio.2.X_CISCO_COM_ApplySetting bool true' >> /var/spool/cron/crontabs/root",
                     iMin, iHour, iDay, iMonth, iGnIndex50);
 #endif
 
