d247b2e0e59bfede7e8fe54a0c0a64e383d58736
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2021-08-02 17:37:07 +0200

    fix ChannelWeight DM parameter is not working as expected in ACS ( OFW-1680 )
    BUG DESCRIPTION:-
    
    GPV retrieves always Zero as channelweight for 2.4 and 5G channels and SPV is failed in ACS.
    
    TESTING PERFOMRED:-
    
    Verified GPV shows value returned from HAL

--- source/TR-181/sbapi/cosa_wifi_apis.c
+++ source/TR-181/sbapi/cosa_wifi_apis.c
@@ -13841,7 +13841,7 @@ PCOSA_DML_WIFI_RADIO_CFG    pCfg        /* Identified by InstanceNumber */
  #endif
     }
 
-#if defined(_LG_MV1_CELENO_)
+#if defined(_LG_MV1_CELENO_) || defined(_LG_MV2_PLUS_)
     if (pStoredCfg->EnhancedACS.DFSMoveBack != pCfg->EnhancedACS.DFSMoveBack)
     {
         wifi_setRadioDfsMoveBackEnable(wlanIndex, pCfg->EnhancedACS.DFSMoveBack);
@@ -14934,7 +14934,7 @@ CosaDmlWiFiRadioGetCfg
 	wifi_getRadioChannel(wlanIndex, pCfgChannel);
 #endif
     
-#if defined(_LG_MV1_CELENO_)
+#if defined(_LG_MV1_CELENO_) || defined(_LG_MV2_PLUS_)
     wifi_getRadioDfsMoveBackEnable(wlanIndex, &pCfg->EnhancedACS.DFSMoveBack);
     wifi_getRadioExcludeDfs(wlanIndex, &pCfg->EnhancedACS.ExcludeDFS);
     memset(pCfg->EnhancedACS.ChannelWeights, 0, sizeof(pCfg->EnhancedACS.ChannelWeights));
