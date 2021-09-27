7d6bde505d3990783af873c8210bcb9f2efc0d86
Hamza Ben Zekri <hbenzekri@libertyglobal.com>
2021-09-27 14:52:24 +0200

    Only send LMLite notification for newly connected STA and not all STAs ( OFW-1945 )
    This fixes a crash that occurs when testing a large number of
    successive station associations using Veriwave. The crash occured
    because of the large number of successive datamodel sets. This is
    because a single station connection caused a notification to be sent
    for each connected station.
    
    Fix: Only send the notification for the newly connected station.

--- source/TR-181/sbapi/cosa_wifi_apis.c
+++ source/TR-181/sbapi/cosa_wifi_apis.c
@@ -17330,7 +17330,7 @@ INT CosaDmlWiFi_AssociatedDevice_callback(INT apIndex, wifi_associated_dev_t *as
 	if(apIndex==0 || apIndex==1 || apIndex==6 || apIndex==7) {	//for private network and guest network
 		if(associated_dev->cli_Active == 1) 
 		{
-			Wifi_Hosts_Sync_Func(NULL,(apIndex+1), associated_dev, 1, 0);
+			Wifi_Hosts_Sync_Func(NULL,(apIndex+1), associated_dev, 0, 0);
 			if ( ANSC_STATUS_SUCCESS == CosaDmlWiFi_GetPreferPrivateData(&bEnabled) )
 			{
 				if (bEnabled == TRUE)
@@ -17341,7 +17341,7 @@ INT CosaDmlWiFi_AssociatedDevice_callback(INT apIndex, wifi_associated_dev_t *as
 		}
 		else 				
 		{
-			Wifi_Hosts_Sync_Func((void *)mac, (apIndex+1), associated_dev, 1, 0);
+			Wifi_Hosts_Sync_Func((void *)mac, (apIndex+1), associated_dev, 0, 0);
 		}
 	}
 #if defined(_CBR_PRODUCT_REQ_) || defined (_BWG_PRODUCT_REQ_)
@@ -17354,11 +17354,11 @@ INT CosaDmlWiFi_AssociatedDevice_callback(INT apIndex, wifi_associated_dev_t *as
 	} else if (apIndex==2 || apIndex==3 ) { //XHS
                 if(associated_dev->cli_Active == 1)
                 {
-                        Wifi_Hosts_Sync_Func(NULL,(apIndex+1), associated_dev, 1, 0);
+                        Wifi_Hosts_Sync_Func(NULL,(apIndex+1), associated_dev, 0, 0);
                 }
                 else
                 {
-                       Wifi_Hosts_Sync_Func((void *)mac,(apIndex+1), associated_dev, 1, 0);
+                       Wifi_Hosts_Sync_Func((void *)mac,(apIndex+1), associated_dev, 0, 0);
                 }
 	}
 #if defined(_CBR_PRODUCT_REQ_) || defined (_BWG_PRODUCT_REQ_)
