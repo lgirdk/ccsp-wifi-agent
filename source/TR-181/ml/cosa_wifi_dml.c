be048c40e8d15ca281ac353bc945848a4b7f9206
Andy Carvalho <acarvalho.contractor@libertyglobal.com>
2021-09-27 16:06:15 +0200

    don't disable WPS when WiFi Security is disabled ( OFW-1912 )
    Fix for: OFW-1912: WPS functionality is disabled, if user disabled security for private 2.4GHz and 5GHz.
    
    BUG DESCRIPTION: WPS getting set to disabled when Wifi security set to disbaled in UI.
    
    FIX DESCRIPTION: removed condition for disabling WPS when wifi security set to disabled.
    
    TESTING PERFORMED:
    1) Set WiFi Security to "Device.WiFi.AccessPoint.1.Security.ModeEnabled" to "None"
    2) Verify "Device.WiFi.AccessPoint.1.WPS.Enable" is set to is true.
    3) Activate WPS with button / set Device.WiFi.AccessPoint.1.WPS.Enable to true
    4) Verify if WPS client able to connect.
    
    Signed-off-by: Andy Carvalho <acarvalho.contractor@libertyglobal.com>

--- source/TR-181/ml/cosa_wifi_dml.c
+++ source/TR-181/ml/cosa_wifi_dml.c
@@ -14509,6 +14509,7 @@ WPS_Validate
 	//Verify whether current security mode is in open state or not
 	wlanIndex = pWifiAp->AP.Cfg.InstanceNumber - 1;
 
+#if 0
 #ifdef WIFI_HAL_VERSION_3
     if (wifiApIsSecmodeOpenForPrivateAP(wlanIndex) != ANSC_STATUS_SUCCESS) {
 
@@ -14538,6 +14539,8 @@ WPS_Validate
                 }
 	}
 #endif //WIFI_HAL_VERSION_3
+#endif
+
     return TRUE;
 }
 
