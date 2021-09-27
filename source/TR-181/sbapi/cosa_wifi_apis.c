dc1944e6cfe61a82ba246d02cc5224fbbd5d134d
Hamza Ben Zekri <hbenzekri@libertyglobal.com>
2021-09-27 14:52:24 +0200

    Only send LMLite notification for newly connected STA and not all STAs ( OFW-1945 )
    This fixes a crash that occurs when testing a large number of
    successive station associations using Veriwave. The crash occured
    because of the large number of successive datamodel sets. This is
    because a single station connection caused a notification to be sent
    for each connected station.
    
    Fix: Only send the notification for the newly connected station.

--- source/TR-181/sbapi/cosa_wifi_apis.c
+++ source/TR-181/sbapi/cosa_wifi_apis.c
@@ -24862,7 +24862,7 @@ INT CosaDmlWiFi_AssociatedDevice_callback(INT apIndex, wifi_associated_dev_t *as
 #endif
 		if(associated_dev->cli_Active == 1) 
 		{
-			Wifi_Hosts_Sync_Func(NULL,(apIndex+1), associated_dev, 1, 0);
+			Wifi_Hosts_Sync_Func(NULL,(apIndex+1), associated_dev, 0, 0);
 			if ( ANSC_STATUS_SUCCESS == CosaDmlWiFi_GetPreferPrivateData(&bEnabled) )
 			{
 				if (bEnabled == TRUE)
@@ -24873,7 +24873,7 @@ INT CosaDmlWiFi_AssociatedDevice_callback(INT apIndex, wifi_associated_dev_t *as
 		}
 		else 				
 		{
-			Wifi_Hosts_Sync_Func((void *)mac, (apIndex+1), associated_dev, 1, 0);
+			Wifi_Hosts_Sync_Func((void *)mac, (apIndex+1), associated_dev, 0, 0);
 		}
 #ifdef WIFI_HAL_VERSION_3
     } else if (isVapHotspot(apIndex)) {
@@ -24893,11 +24893,11 @@ INT CosaDmlWiFi_AssociatedDevice_callback(INT apIndex, wifi_associated_dev_t *as
 
                 if(associated_dev->cli_Active == 1)
                 {
-                        Wifi_Hosts_Sync_Func(NULL,(apIndex+1), associated_dev, 1, 0);
+                        Wifi_Hosts_Sync_Func(NULL,(apIndex+1), associated_dev, 0, 0);
                 }
                 else
                 {
-                       Wifi_Hosts_Sync_Func((void *)mac,(apIndex+1), associated_dev, 1, 0);
+                       Wifi_Hosts_Sync_Func((void *)mac,(apIndex+1), associated_dev, 0, 0);
                 }	
 #ifdef WIFI_HAL_VERSION_3  
     } else if (isVapLnf(apIndex)) { //L&F
