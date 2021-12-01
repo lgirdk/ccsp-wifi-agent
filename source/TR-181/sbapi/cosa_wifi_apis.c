94f3295a8328b5c21ed38e48a0905c04e546b9d0
Tarun Saha <tsaha@libertyglobal.com>
2021-11-30 17:25:35 -0800

    add support for Radio.RetryLimit ( OFW-2086 )
    Added set/get api for Radio.RetryLimit parameter

--- source/TR-181/sbapi/cosa_wifi_apis.c
+++ source/TR-181/sbapi/cosa_wifi_apis.c
@@ -14097,6 +14097,10 @@ PCOSA_DML_WIFI_RADIO_CFG    pCfg        /* Identified by InstanceNumber */
         wlanRestart = TRUE;
     }
 
+    if (pStoredCfg->RetryLimit != pCfg->RetryLimit)
+    {
+        wifi_setRadioRetryLimit(wlanIndex, pCfg->RetryLimit);
+    }
     wifi_getRadioChannel(wlanIndex, &runningChannel);
     if (pCfg->AutoChannelEnable != pStoredCfg->AutoChannelEnable)
     {
@@ -15162,6 +15166,9 @@ CosaDmlWiFiRadioGetCfg
 //>> zqiu	
 	CosaDmlWiFiGetRadioStandards(wlanIndex, pCfg->OperatingFrequencyBand, pCfgOperatingStandards);
 //<<
+
+	wifi_getRadioRetryLimit(wlanIndex, &pCfg->RetryLimit);
+
 	pCfgChannel = &pCfg->Channel;
 #if defined(_LG_MV1_CELENO_)
 	wifi_getRadioConfiguredChannel(wlanIndex, pCfgChannel);
