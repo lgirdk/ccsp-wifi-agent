074ba1361dcbb67b224d09011d577339fb6ba6a6
Devin Mungan <dmungan@libertyglobal.com>
2021-12-07 15:24:01 +0100

    ensure RM Enabled Capability is disabled when Opensync is turned off ( OFW-2155 CH7465SI-85 )
    [Issue]
    The 'RM enabled capabilities' IE should only be present if SON / Opensync is configured and enabled.
    
    [Solution]
    1. Implement get/set of "X_RDKCENTRAL-COM_NeighborReportActivated" properly and also check the cfg
    value before setting it to the cfg since the agent cache won't be updated if Opensync changes the
    setting via HAL layer	.
    2. Do not use PSM for RRM state setting decisions. CFG will be used instead.
    
    Signed-off-by: Devin Mungan(dmungan@libertyglobal.com)
    
    avoid multiple radio restarts during bootup on Mv1 when Plume is enabled ( OFW-2565 )
    
    [Issue]
    Neighbor report activation setting shall be stored in cfg rather than PSM. Otherwise,
    setting inconsistency between PSM and cfg triggers another wifi apply at bootup.
    
    [Solution]
    Completely detach neighbor report setting from PSM. At bootup, read the initial value
    of this setting from cfg via corresponding using WiFi HAL API.
    
    [Testing Performed]
    1. Enable Plume
    2. Reboot the device.
    3. Verify that only one apply is realized for each radios
    
    Signed-off-by: Devin Mungan(dmungan@libertyglobal.com)

--- source/TR-181/ml/cosa_wifi_dml.c
+++ source/TR-181/ml/cosa_wifi_dml.c
@@ -9006,12 +9006,19 @@ AccessPoint_GetParamBoolValue
     if (strcmp(ParamName, "X_RDKCENTRAL-COM_NeighborReportActivated") == 0)
     {
         /* collect value */
-#if defined(_COSA_BCM_MIPS_) || defined(_CBR_PRODUCT_REQ_) || defined(HUB4_WLDM_SUPPORT)
         INT wlanIndex = -1;
         wlanIndex = pWifiAp->AP.Cfg.InstanceNumber -1 ;
-        wifi_getNeighborReportActivation(wlanIndex , &pWifiAp->AP.Cfg.X_RDKCENTRAL_COM_NeighborReportActivated);
-#endif
-        *pBool = pWifiAp->AP.Cfg.X_RDKCENTRAL_COM_NeighborReportActivated;
+        BOOL rrmEnabled;
+
+        if (wifi_getNeighborReportActivation(wlanIndex , &rrmEnabled) != RETURN_OK)
+        {
+            *pBool = pWifiAp->AP.Cfg.X_RDKCENTRAL_COM_NeighborReportActivated;
+        }
+        else
+        {
+            *pBool = rrmEnabled;
+        }
+
         return TRUE;
     }
 
@@ -9979,8 +9986,12 @@ AccessPoint_SetParamBoolValue
         pWifiAp->AP.isApChanged = TRUE;
         return TRUE;
 #else
-        /* collect value */
-        if ( pWifiAp->AP.Cfg.X_RDKCENTRAL_COM_NeighborReportActivated == bValue )
+        BOOL rrmEnabled;
+
+        wifi_getNeighborReportActivation(pWifiAp->AP.Cfg.InstanceNumber - 1, &rrmEnabled);
+
+        if (pWifiAp->AP.Cfg.X_RDKCENTRAL_COM_NeighborReportActivated == bValue &&
+            pWifiAp->AP.Cfg.X_RDKCENTRAL_COM_NeighborReportActivated == rrmEnabled)
         {
             pWifiAp->bApChanged = FALSE;
             return  TRUE;
@@ -9991,9 +10002,9 @@ AccessPoint_SetParamBoolValue
         {
             /* save update to backup */
             pWifiAp->AP.Cfg.X_RDKCENTRAL_COM_NeighborReportActivated = bValue;
-            pWifiAp->bApChanged = FALSE;
-            return TRUE;
+            pWifiAp->bApChanged = TRUE;
         }		
+        return TRUE;
 #endif
     }
 
