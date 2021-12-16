f74c3a6e671ac1916de6b092925c011c077588e6
Shravan Hiremath <shhiremath.contractor.contractor@libertyglobal.com>
2021-12-16 08:28:52 +0100

    update Device.WiFi.AccessPoint.{i}.Security.X_LGI-COM_Radius.ReAuthInterval etc type from int to uint ( OFW-2151 F3896SI-598 )
    Fix SPV of writable TR181 data model results in 9006 - Invalid parameter type error.
    
    [Description]
    
      Device.WiFi.AccessPoint.{i}.Security.X_LGI-COM_Radius.PMKCacheInterval
      Device.WiFi.AccessPoint.{i}.Security.X_LGI-COM_Radius.ReAuthInterval
    
    The above parameters were implemented as data-type int instead of data-type unsignedInt as per MVXREQ.
    
    [Fix]
    
    Changed the data-type to unsignedInt. Implemented the functions GetParamUlongValue / SetParamUlongValue.
    
    [Testing Performed]
    
    1) dmcli eRT getv of the params returns data-type as uint.
    2) Execution of dmcli eRT setv succeeds when data-type used is uint and fails with other data-type.
    
    Signed-off-by: Shravan Hiremath <shhiremath.contractor.contractor@libertyglobal.com>
    
    fix Device.WiFi.AccessPoint.{i}.Security.X_LGI-COM_Radius.PMKLifetime data type ( OFW-2241 )
    
    Fix SPV returns Invalid Parameter Type.
    
    [Description]
    
    Device.WiFi.AccessPoint.{i}.Security.X_LGI-COM_Radius.PMKLifetime
    
    The above parameter is implemented as data-type int instead of unsignedInt as per MVXREQ.
    
    [Fix]
    
    Changed the parameter data-type to unsignedInt.
    
    [Testing Performed]
    
    1) dmcli eRT getv of the param returns data-type as uint.
    2) Execution of dmcli eRT setv succeeds when data-type used is uint and fails with other data-type.
    
    Signed-off-by: Shravan Hiremath <shhiremath.contractor.contractor@libertyglobal.com>

--- source/TR-181/ml/cosa_wifi_dml.c
+++ source/TR-181/ml/cosa_wifi_dml.c
@@ -20593,6 +20593,60 @@ RadiusSettings_GetParamBoolValue
 }
 
 BOOL
+RadiusSettings_GetParamUlongValue
+    (
+        ANSC_HANDLE                 hInsContext,
+        char*                       ParamName,
+        ULONG*                      puLong
+    )
+{
+    PCOSA_DATAMODEL_WIFI            pMyObject    = (PCOSA_DATAMODEL_WIFI     )g_pCosaBEManager->hWifi;
+    PCOSA_CONTEXT_LINK_OBJECT       pLinkObj     = (PCOSA_CONTEXT_LINK_OBJECT)hInsContext;
+    PCOSA_DML_WIFI_AP               pWifiAp      = (PCOSA_DML_WIFI_AP        )pLinkObj->hContext;
+    PSINGLE_LINK_ENTRY              pSLinkEntry  = (PSINGLE_LINK_ENTRY       )NULL;
+    PCOSA_CONTEXT_LINK_OBJECT       pSSIDLinkObj = (PCOSA_CONTEXT_LINK_OBJECT)NULL;
+    CHAR                            PathName[64];
+
+    pSLinkEntry = AnscQueueGetFirstEntry(&pMyObject->SsidQueue);
+
+    while ( pSLinkEntry )
+    {
+        pSSIDLinkObj = ACCESS_COSA_CONTEXT_LINK_OBJECT(pSLinkEntry);
+
+        sprintf(PathName, "Device.WiFi.SSID.%lu.", pSSIDLinkObj->InstanceNumber);
+        if ( AnscEqualString(pWifiAp->AP.Cfg.SSID, PathName, TRUE) )
+        {
+            break;
+        }
+
+        pSLinkEntry             = AnscQueueGetNextEntry(pSLinkEntry);
+    }
+
+    if (strcmp(ParamName, "PMKCacheInterval") == 0)
+    {
+        /* collect value */
+        *puLong = pWifiAp->AP.RadiusSetting.iPMKCacheInterval;
+        return TRUE;
+    }
+
+    if (strcmp(ParamName, "ReAuthInterval") == 0)
+    {
+        /* collect value */
+        *puLong = pWifiAp->AP.RadiusSetting.iReAuthInterval;
+        return TRUE;
+    }
+
+    if (strcmp(ParamName, "PMKLifetime") == 0)
+    {
+        /* collect value */
+        *puLong = pWifiAp->AP.RadiusSetting.iPMKLifetime;
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+BOOL
 RadiusSettings_GetParamIntValue
     (
         ANSC_HANDLE                 hInsContext,
@@ -20646,27 +20700,6 @@ RadiusSettings_GetParamIntValue
     {
         /* collect value */
         *pInt = pWifiAp->AP.RadiusSetting.iRadiusServerRequestTimeout;
-        return TRUE;
-    }
-
-    if (strcmp(ParamName, "PMKLifetime") == 0)	
-    {
-        /* collect value */
-        *pInt = pWifiAp->AP.RadiusSetting.iPMKLifetime;
-        return TRUE;
-    }
-
-    if (strcmp(ParamName, "PMKCacheInterval") == 0)
-    {
-        /* collect value */
-        *pInt = pWifiAp->AP.RadiusSetting.iPMKCacheInterval;
-        return TRUE;
-    }
-
-    if (strcmp(ParamName, "ReAuthInterval") == 0)
-    {
-        /* collect value */
-        *pInt = pWifiAp->AP.RadiusSetting.iReAuthInterval;
         return TRUE;
     }
 
@@ -20735,6 +20768,52 @@ return FALSE;
 }
 
 BOOL
+RadiusSettings_SetParamUlongValue
+    (
+        ANSC_HANDLE                 hInsContext,
+        char*                       ParamName,
+        ULONG                       uValue
+    )
+{
+    PCOSA_CONTEXT_LINK_OBJECT       pLinkObj     = (PCOSA_CONTEXT_LINK_OBJECT)hInsContext;
+    PCOSA_DML_WIFI_AP               pWifiAp      = (PCOSA_DML_WIFI_AP        )pLinkObj->hContext;
+
+    AnscTraceWarning(("ParamName: %s uValue: %u\n", ParamName, uValue));
+	
+    if (strcmp(ParamName, "PMKCacheInterval") == 0)
+    {
+        if(pWifiAp->AP.RadiusSetting.bPMKCaching == FALSE)
+        {
+            CcspTraceWarning(("'%s' cannot be set as PMKCaching is set to False\n", ParamName));
+            return FALSE;
+        }
+
+        /* save update to backup */
+        pWifiAp->AP.RadiusSetting.iPMKCacheInterval = uValue;
+        return TRUE;
+    }
+
+    if (strcmp(ParamName, "ReAuthInterval") == 0)
+    {
+        /* save update to backup */
+        if(uValue != pWifiAp->AP.RadiusSetting.iReAuthInterval)
+        {
+              pWifiAp->AP.RadiusSetting.iReAuthInterval = uValue;
+        }
+        return TRUE;
+    }
+
+    if (strcmp(ParamName, "PMKLifetime") == 0)
+    {
+        /* save update to backup */
+        pWifiAp->AP.RadiusSetting.iPMKLifetime = uValue;
+        return TRUE;
+    }
+	
+    return FALSE;
+}
+
+BOOL
 RadiusSettings_SetParamIntValue
     (
         ANSC_HANDLE                 hInsContext,
@@ -20780,33 +20859,6 @@ RadiusSettings_SetParamIntValue
     {
         /* save update to backup */
         pWifiAp->AP.RadiusSetting.iRadiusServerRequestTimeout = iValue;
-        return TRUE;
-    }
-
-    if (strcmp(ParamName, "PMKLifetime") == 0)
-    {
-        /* save update to backup */
-        pWifiAp->AP.RadiusSetting.iPMKLifetime = iValue;
-        return TRUE;
-    }
-
-    if (strcmp(ParamName, "PMKCacheInterval") == 0)
-    {
-        if(pWifiAp->AP.RadiusSetting.bPMKCaching == FALSE)
-        {
-            CcspTraceWarning(("'%s' cannot be set as PMKCaching is set to False\n", ParamName));
-            return FALSE;
-        }
-
-        /* save update to backup */
-        pWifiAp->AP.RadiusSetting.iPMKCacheInterval = iValue;
-        return TRUE;
-    }
-
-    if (strcmp(ParamName, "ReAuthInterval") == 0)
-    {
-        /* save update to backup */
-        pWifiAp->AP.RadiusSetting.iReAuthInterval = iValue;
         return TRUE;
     }
 
