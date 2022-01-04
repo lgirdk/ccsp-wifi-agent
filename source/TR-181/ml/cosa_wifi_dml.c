fda49aa611f53e02c121b69cfef13f5a0f2d468f
Shravan Hiremath <shhiremath.contractor.contractor@libertyglobal.com>
2022-01-04 08:04:59 +0100

    fix Device.WiFi.AccessPoint.{i}.Security.X_LGI-COM_Radius.PMKLifetime data type ( OFW-2241 )
    Fix SPV returns Invalid Parameter Type.
    
    [Description]
    
    Device.WiFi.AccessPoint.{i}.Security.X_LGI-COM_Radius.PMKLifetime
    
    The above parameter is implemented as data-type int instead of unsignedInt as per MVXREQ.
    
    [Fix]
    
    Changed the parameter data-type to unsignedInt.
    
    [Testing Performed]
    
    1) dmcli eRT getv of the param returns data-type as uint.
    2) Execution of dmcli eRT setv succeeds when data-type used is uint and fails with other data-type.
    
    Signed-off-by: Shravan Hiremath <shhiremath.contractor.contractor@libertyglobal.com>

--- source/TR-181/ml/cosa_wifi_dml.c
+++ source/TR-181/ml/cosa_wifi_dml.c
@@ -21117,6 +21117,13 @@ RadiusSettings_GetParamUlongValue
         return TRUE;
     }
 
+    if (strcmp(ParamName, "PMKLifetime") == 0)
+    {
+        /* collect value */
+        *puLong = pWifiAp->AP.RadiusSetting.iPMKLifetime;
+        return TRUE;
+    }
+
     return FALSE;
 }
 
@@ -21174,13 +21181,6 @@ RadiusSettings_GetParamIntValue
     {
         /* collect value */
         *pInt = pWifiAp->AP.RadiusSetting.iRadiusServerRequestTimeout;
-        return TRUE;
-    }
-
-    if (strcmp(ParamName, "PMKLifetime") == 0)	
-    {
-        /* collect value */
-        *pInt = pWifiAp->AP.RadiusSetting.iPMKLifetime;
         return TRUE;
     }
 
@@ -21283,6 +21283,13 @@ RadiusSettings_SetParamUlongValue
         }
         return TRUE;
     }
+
+    if (strcmp(ParamName, "PMKLifetime") == 0)
+    {
+        /* save update to backup */
+        pWifiAp->AP.RadiusSetting.iPMKLifetime = uValue;
+        return TRUE;
+    }
 	
     return FALSE;
 }
@@ -21333,13 +21340,6 @@ RadiusSettings_SetParamIntValue
     {
         /* save update to backup */
         pWifiAp->AP.RadiusSetting.iRadiusServerRequestTimeout = iValue;
-        return TRUE;
-    }
-
-    if (strcmp(ParamName, "PMKLifetime") == 0)
-    {
-        /* save update to backup */
-        pWifiAp->AP.RadiusSetting.iPMKLifetime = iValue;
         return TRUE;
     }
 
