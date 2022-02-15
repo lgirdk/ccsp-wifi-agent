7189b18ff076dcc7bbac4da1af1878f7ce82f1ad
Saravanakumar Karthikeyan <skarthikeyan.contractor@libertyglobal.com>
2022-02-15 10:46:02 +0530

    disable unnecessary debug to improve performance ( OFW-2282 CH7465SI-154 )
    FIX DESCRIPTION:
    
    Removed some of the unwanted bootup print logs.
    
    TESTING PERFORMED:
    
    Removed print logs are not present in /rdklogs/logs after bootup.
    
    Signed-off-by: Saravanakumar Karthikeyan <skarthikeyan.contractor@libertyglobal.com>

--- source/TR-181/sbapi/cosa_wifi_apis.c
+++ source/TR-181/sbapi/cosa_wifi_apis.c
@@ -24821,6 +24821,7 @@ void *updateBootLogTime() {
             wifi_getApStatus( 10 , output_AP10 );
             wifi_getApStatus( 11 , output_AP11 );
 
+#if 0
             CcspTraceWarning(("%s-%d LnF SSID 6:%s 7:%s 10:%s 11:%s\n",
                         __FUNCTION__,
                         __LINE__,
@@ -24828,6 +24829,7 @@ void *updateBootLogTime() {
                         output_AP7,
                         output_AP10,
                         output_AP11 ));
+#endif
 
             if(( 0 == strcmp( output_AP6 ,"Up" ) ) || \
                     ( 0 == strcmp( output_AP7 ,"Up" ) ) || \
@@ -24896,6 +24898,7 @@ void *updateBootLogTime() {
             wifi_getApStatus( 8 , output_AP8 );
             wifi_getApStatus( 9 , output_AP9 );
 
+#if 0
             CcspTraceWarning(("%s-%d Xfinity SSID 4:%s 5:%s 8:%s 9:%s\n",
                                     __FUNCTION__,
                                     __LINE__,
@@ -24903,6 +24906,7 @@ void *updateBootLogTime() {
                                     output_AP5,
                                     output_AP8,
                                     output_AP9 ));
+#endif
 
             if(( 0 == strcmp( output_AP4 ,"Up" ) ) || \
                 ( 0 == strcmp( output_AP5 ,"Up" ) ) || \
