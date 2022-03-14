ad64441e4832e25230e9797838d0d7f5238cea72
Karthik Shanbhag <kshanbhag@libertyglobal.com>
2022-03-14 17:44:36 +0100

    add data model support for Device.WiFi.AccessPoint.1.X_LGI-COM_MacFilterTable.{i}.Description
    [Description]
        Add new Description datamodel object:
        Device.WiFi.AccessPoint.1.X_LGI-COM_MacFilterTable.{i}.Description
    
    Signed-off-by: Karthik Shanbhag(kshanbhag@libertyglobal.com)

--- source/TR-181/ml/cosa_wifi_dml.c
+++ source/TR-181/ml/cosa_wifi_dml.c
@@ -20638,6 +20638,12 @@ MacFiltTab_GetParamStringValue
         AnscCopyString(pValue, pMacFilt->DeviceName);
         return 0;
     }
+    if (strcmp(ParamName, "Description") == 0)
+    {
+        /* collect value */
+        AnscCopyString(pValue, pMacFilt->Description);
+        return 0;
+    }
 
     /* CcspTraceWarning(("Unsupported parameter '%s'\n", ParamName)); */
     return -1;
@@ -20669,6 +20675,15 @@ MacFiltTab_SetParamStringValue
         if (AnscSizeOfString(pString) >= sizeof(pMacFilt->DeviceName))
             return FALSE;
         AnscCopyString(pMacFilt->DeviceName, pString);
+        return TRUE;
+    }
+
+    if (strcmp(ParamName, "Description") == 0)
+    {
+        /* save update to backup */
+        if (AnscSizeOfString(pString) >= sizeof(pMacFilt->Description))
+            return FALSE;
+        AnscCopyString(pMacFilt->Description, pString);
         return TRUE;
     }
 
