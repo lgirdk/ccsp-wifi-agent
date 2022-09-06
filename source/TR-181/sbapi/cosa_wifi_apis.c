f39f5f34cddfb7f90bd40db7082843cce5dc17fd
esrikanth <esrikanth.contractor@libertyglobal.com>
2022-09-06 14:42:27 +0530

    fix connected Devices and DHCP pages are not loading after changing IPv6LANMode configuration ( OFW-3039 )
    [Issue]
    [Mv2+] Connected Devices and DHCP page are not loading after Kicking all associated devices
    
    [Solution]
    Use wifi_getApAssociatedDevice to get all associated devies instead of using the list returned by CosaDmlWiFiApGetAssocDevices
    
    [Testing Performed]
     1. Ensure DUT is online
     2. Connect two clients to 5G private network.
     3. Make sure host table is proper (dmcli eRT getv Device.Hosts.Host.)
     4. Kick all associated devices (dmcli eRT setv Device.WiFi.AccessPoint.2.X_CISCO_COM_KickAssocDevices bool true)
     5. Connected Devices and DHCP page on GUI should load with correct information
    
    Upstream Candidate: No. LGI specific fix
    
    Signed-off-by: esrikanth <esrikanth.contractor@libertyglobal.com>

--- source/TR-181/sbapi/cosa_wifi_apis.c
+++ source/TR-181/sbapi/cosa_wifi_apis.c
@@ -24678,7 +24678,7 @@ void *Wifi_Hosts_Sync_Func(void *pt, int index, wifi_associated_dev_t *associate
 #endif
 	BOOL enabled=FALSE;
         int rc = -1;
-#if defined(_INTEL_BUG_FIXES_)
+#if defined(_INTEL_BUG_FIXES_) || !defined(_LG_MV1_CELENO_)
 	char *output_buf = NULL;
 	char partOfAddress[3];
 #else
@@ -24717,7 +24717,7 @@ void *Wifi_Hosts_Sync_Func(void *pt, int index, wifi_associated_dev_t *associate
 
 			count = 0;
 
-#if !defined(_INTEL_BUG_FIXES_)
+#if !defined(_INTEL_BUG_FIXES_) && defined(_LG_MV1_CELENO_)
                         assoc_devices = CosaDmlWiFiApGetAssocDevices(NULL, ssid , &count);
                         if (assoc_devices == NULL)
                         {
@@ -24763,7 +24763,11 @@ void *Wifi_Hosts_Sync_Func(void *pt, int index, wifi_associated_dev_t *associate
 					for (j=0; j < 6; j++)
 					{
 						snprintf(partOfAddress, 3, "%s", &output_buf[i*18 + j*3]);
+#if defined(_LG_MV1_CELENO_)
 						assoc_devices[i].MacAddress[j] = (char)strtol(partOfAddress, NULL, 16);
+#else
+						assoc_devices[count-i-1].MacAddress[j] = (char)strtol(partOfAddress, NULL, 16);
+#endif
 					}
 				}
 			}
@@ -24792,7 +24796,7 @@ void *Wifi_Hosts_Sync_Func(void *pt, int index, wifi_associated_dev_t *associate
 			_ansc_snprintf(ssid,sizeof(ssid),"Device.WiFi.SSID.%d",index);
 
 
-#if !defined(_INTEL_BUG_FIXES_)
+#if !defined(_INTEL_BUG_FIXES_) && defined(_LG_MV1_CELENO_)
                 for(j = 0; j < count ; j++)
                 {
                         //CcspWifiTrace(("RDK_LOG_WARN,WIFI-CLIENT <%s> <%d> : j = %d \n",__FUNCTION__, __LINE__ , j));
@@ -24957,7 +24961,7 @@ void *Wifi_Hosts_Sync_Func(void *pt, int index, wifi_associated_dev_t *associate
 #endif
 			count = 0;
 
-#if !defined(_INTEL_BUG_FIXES_)
+#if !defined(_INTEL_BUG_FIXES_) && defined(_LG_MV1_CELENO_)
                         assoc_devices = CosaDmlWiFiApGetAssocDevices(NULL, ssid , &count);
                         if (assoc_devices == NULL)
                         {
@@ -25073,7 +25077,7 @@ void *Wifi_Hosts_Sync_Func(void *pt, int index, wifi_associated_dev_t *associate
 #endif
 	}
 
-#if defined(_INTEL_BUG_FIXES_)
+#if defined(_INTEL_BUG_FIXES_) || !defined(_LG_MV1_CELENO_)
 	if (output_buf)
         {
 		AnscFreeMemory(output_buf);
