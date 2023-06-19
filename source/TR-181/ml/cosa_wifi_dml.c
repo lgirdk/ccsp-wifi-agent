7b33b6fe622147631547a88b8daadcafe6310524
Keerthana <kvijayakumarjalajaku.contractor@libertyglobal.com>
2023-06-19 22:44:25 +0200

    fix WiFi ChannelWeight Set failed(not saved to nvram) via TR-181 data model ( F3896SI-2008 ) (OFW-4594)
    BUG DESCRIPTION:
    WiFi ChannelWeight Set via dmcli command is successful, but failed save to nvram.
    The code to set channel weight to nvram is missing when HAL_V3 is introduced.
    
    FIX DESCRIPTION:
    The missing code has been added to execute even if HAL_V3 is enabled.
    It has been identified that there are other two parameters "DFSMoveBack"
    and "ExcludeDFS" also has the same issue. The missing code has been added
    under HAL_V3 flag for those two additional parameters as well.
    
    TESTING PERFORMED:
    Tested and ensure that the updated channel weight is also getting applied to nvram.
    1. Get the channel number of 2.4 /5G radio
    2. Update the channel weight for that particular channel
    3. GPV of channel weight is returning the updated value
    4. Get the channel weight from nvram - nvram get wl1_acs_channel_weights , also returns the updated value.
    
    DFS Move back:
    1. Get the DFS Move back for the radio index 5G
    2. Update the value.
    3. Ensure GPV also returns the same updated value
    4. Ensure the wifi api - wifi_getRadioDfsMoveBackEnable returns the updated value
    Similarly for ExcludeDFS
    
    Upstreamable : Yes
    
    Signed-off-by: Keerthana V J(kvijayakumarjalajaku.contractor@libertyglobal.com)

--- source/TR-181/ml/cosa_wifi_dml.c
+++ source/TR-181/ml/cosa_wifi_dml.c
@@ -6350,7 +6350,13 @@ EnhancedACS_SetParamBoolValue
         return ANSC_STATUS_FAILURE;
     }
     if (strcmp(ParamName, "DFSMoveBack") == 0) {
-        pWifiRadioEnhancedACS->DFSMoveBack = bValue;
+        if(pWifiRadioEnhancedACS->DFSMoveBack != bValue)
+        {
+            pWifiRadioEnhancedACS->DFSMoveBack = bValue;
+#ifdef WIFI_HAL_VERSION_3
+            pWifiRadioCfg->isRadioConfigChanged = TRUE;
+#endif
+        }
         return TRUE;
     }
     if (strcmp(ParamName, "ExcludeDFS") == 0) {
@@ -6360,6 +6366,9 @@ EnhancedACS_SetParamBoolValue
                 return FALSE;
             }
             pWifiRadioEnhancedACS->ExcludeDFS = bValue;
+#ifdef WIFI_HAL_VERSION_3
+            pWifiRadioCfg->isRadioConfigChanged = TRUE;
+#endif
         }
         return TRUE;
     }
@@ -6481,7 +6490,7 @@ static BOOL isWeatherChannel(ULONG channel) {
     }
 }
 
-static ANSC_STATUS GetInsNumsByWifiChannelWeight( ULONG* pWeight, ULONG *channel, BOOL *excludeDFS, COSA_DML_WIFI_CHAN_BW *bandwidth)
+static ANSC_STATUS GetInsNumsByWifiChannelWeight( ULONG* pWeight, ULONG *channel, BOOL *excludeDFS, COSA_DML_WIFI_CHAN_BW *bandwidth,bool isOperSPV, ULONG uValue)
 {
     PCOSA_DATAMODEL_WIFI        pWiFi       = (PCOSA_DATAMODEL_WIFI)g_pCosaBEManager->hWifi;
     PCOSA_DML_WIFI_RADIO            pWifiRadio      = NULL;
@@ -6506,6 +6515,12 @@ static ANSC_STATUS GetInsNumsByWifiChannelWeight( ULONG* pWeight, ULONG *channel
                 *channel = (wlanIndex == 0)? channels24G[i]: channels5G[i];
                 *excludeDFS = pWifiRadioEnhancedACS->ExcludeDFS;
                 *bandwidth = pWifiRadioCfg->OperatingChannelBandwidth;
+#ifdef WIFI_HAL_VERSION_3
+                if(isOperSPV && (uValue != *pWeight))
+                {
+                    pWifiRadioCfg->isRadioConfigChanged = TRUE;
+                }
+#endif
                 return ANSC_STATUS_SUCCESS;
             }
         }
@@ -6530,7 +6545,7 @@ Channel_GetParamUlongValue
 
     if (strcmp(ParamName, "ChannelWeight") == 0)
     {
-        GetInsNumsByWifiChannelWeight(pWeight, &channel, &excludeDFS, &bandwidth);
+        GetInsNumsByWifiChannelWeight(pWeight, &channel, &excludeDFS, &bandwidth, false, 0);
         if (excludeDFS && isDFSChannel(channel)) {
             *puLong = 0;
         } else {
@@ -6556,7 +6571,7 @@ Channel_SetParamUlongValue
 
     if (strcmp(ParamName, "ChannelWeight") == 0)
     {
-        GetInsNumsByWifiChannelWeight(pWeight, &channel, &excludeDFS, &bandwidth);
+        GetInsNumsByWifiChannelWeight(pWeight, &channel, &excludeDFS, &bandwidth, true, uValue);
         if ((channel >= 36 && channel <= 140 && !isWeatherChannel(channel)) || channel == 1 || channel == 6 || channel == 11) {
             *pWeight = uValue;
         }
--- source/TR-181/sbapi/cosa_wifi_apis.c
+++ source/TR-181/sbapi/cosa_wifi_apis.c
@@ -12732,6 +12732,7 @@ PCOSA_DML_WIFI_RADIO_CFG    pCfg        /* Identified by InstanceNumber */
     PCOSA_DML_WIFI_SSID             pWifiSsid           = (PCOSA_DML_WIFI_SSID      )NULL;
     PCOSA_DML_WIFI_AP               pWifiAp             = (PCOSA_DML_WIFI_AP        )NULL;
     PCOSA_CONTEXT_LINK_OBJECT       pLinkObj            = (PCOSA_CONTEXT_LINK_OBJECT)NULL;
+    PCOSA_DML_WIFI_RADIO_CFG        pStoredCfg          = (PCOSA_DML_WIFI_RADIO_CFG)NULL;
 
     UNREFERENCED_PARAMETER(sWiFiDmlRadioRunningCfg);
     UNREFERENCED_PARAMETER(sWiFiDmlWepChg);
@@ -12742,6 +12743,7 @@ PCOSA_DML_WIFI_RADIO_CFG    pCfg        /* Identified by InstanceNumber */
         return ANSC_STATUS_FAILURE;
     }
 
+    pStoredCfg = &sWiFiDmlRadioStoredCfg[pCfg->InstanceNumber-1];
     instanceNumber = pCfg->InstanceNumber;
     if ((instanceNumber == 0) || (instanceNumber > getNumberRadios()))
     {
@@ -12848,6 +12850,23 @@ PCOSA_DML_WIFI_RADIO_CFG    pCfg        /* Identified by InstanceNumber */
             tmpWifiVapInfoMap.num_vaps = vapArrayIndex;
         }
         isVapApply = FALSE;
+    }
+
+    if (pStoredCfg->EnhancedACS.DFSMoveBack != pCfg->EnhancedACS.DFSMoveBack)
+    {
+        wifi_setRadioDfsMoveBackEnable(radioIndex, pCfg->EnhancedACS.DFSMoveBack);
+    }
+
+    if (pStoredCfg->EnhancedACS.ExcludeDFS != pCfg->EnhancedACS.ExcludeDFS)
+    {
+        wifi_setRadioExcludeDfs(radioIndex, pCfg->EnhancedACS.ExcludeDFS);
+    }
+
+    if (memcmp(pStoredCfg->EnhancedACS.ChannelWeights, pCfg->EnhancedACS.ChannelWeights, sizeof(pCfg->EnhancedACS.ChannelWeights)))
+    {
+        ULONG halWeights[sizeof(pCfg->EnhancedACS.ChannelWeights)] = {0};
+        fromChannelWeightsToHalWeights(radioIndex, pCfg->EnhancedACS.ChannelWeights, halWeights, sizeof(pCfg->EnhancedACS.ChannelWeights));
+        wifi_setRadioChannelWeights(radioIndex, halWeights);
     }
 
     //If Validation is successful, apply the radio and VAP settings
