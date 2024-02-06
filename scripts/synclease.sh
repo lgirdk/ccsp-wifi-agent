9659d06410a10a18271698aa5cca8bfae79d1f33
Andre McCurdy <armccurdy@gmail.com>
2024-02-06 11:17:55 -0800

    synclease.sh: source /etc/device.properties rather than using grep etc
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- scripts/synclease.sh
+++ scripts/synclease.sh
@@ -4,13 +4,13 @@
 #for the first time when mesh is enabled, also during plume agent
 #restart
 
-ARM_IP=`grep "ARM_INTERFACE_IP" /etc/device.properties | cut -d "=" -f2`
+. /etc/device.properties
 
-if [ -n "$ARM_IP" ]; then
+if [ -n "${ARM_INTERFACE_IP}" ]; then
     if [ ! -f /tmp/login.swr ]; then
         configparamgen jx /etc/dropbear/elxrretyt.swr /tmp/login.swr
     fi
-    scp -i /tmp/login.swr root@$ARM_IP:/nvram/dnsmasq.leases /tmp/dnsmasq.leases
+    scp -i /tmp/login.swr root@${ARM_INTERFACE_IP}:/nvram/dnsmasq.leases /tmp/dnsmasq.leases
 else
     tmpfile=$(mktemp /tmp/dnsmasq.leases.XXXXXX)
     flock /tmp/.dnsmasq_leases_lock cp /nvram/dnsmasq.leases $tmpfile
