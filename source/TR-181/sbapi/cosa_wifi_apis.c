1b98ae1c903a585bdfce928c64f2188be6d5407d
sreesreekumar <ssreekumar.contractor@libertyglobal.com>
2024-08-05 21:50:23 +0530

    fix wifi client should not connect to guest wifi network when Wireless MAC filter set to Deny ( OFW-5983 TG2492SI-431 )
    [Issue]
    Wifi clients will connect to guest network if we set deny in Wireless Mac Filter.
    
    [Decription]
    Mac address is not added in the guest maclist in config files (cfg -s | grep AP_MACLIST)
    
    [Solution]
    Updated the code to add MAcaddress in the MACList for guestnetwork also.
    
    [Testing]
    1. Load the DUT in latest Firmware
    2. Connect to the 5G Private Wifi network.
    3. Navigate to Advanced --> Wireless --> Security --> Wireless MAC Filtering
    4. Check the connected wifi client MAC address
    5. Add the Wireless MAC Filtering rule of connected wifi MAC address with Deny option for both 2.4GHz and 5GHz
    6. Connected Wifi client should be disconnected.
    7. Navigate to Advanced --> Wireless -->Guest Network. Enable Guest network and click apply changes
    8. Verified Guest network not connected
    
    Signed-off-by: Sreeja Sreekumar (ssreekumar.contractor@libertyglobal.com)

--- source/TR-181/sbapi/cosa_wifi_apis.c
+++ source/TR-181/sbapi/cosa_wifi_apis.c
@@ -22003,7 +22003,6 @@ wifiDbgPrintf("%s\n",__FUNCTION__);
     int retPsmGet = CCSP_SUCCESS;
     int retPsmSet = CCSP_SUCCESS;
     char *strValue = NULL;
-	BOOL enabled=FALSE;
 #if defined(ENABLE_FEATURE_MESHWIFI)
     CHAR multinet_instance[128] = {0};
 #endif
@@ -22025,8 +22024,8 @@ wifiDbgPrintf("%s\n",__FUNCTION__);
         }
 
 	pthread_mutex_lock(&MacFilt_CountMutex);
-	wifi_getApEnable(apIns-1, &enabled);
-	if (enabled) { 		 			
+
+	{
 		CcspTraceWarning(("Mac Filter Entry count:%d\n", g_macFiltCnt[apIns-1]));
 		int rc = wifi_addApAclDevice(apIns-1,pMacFilt->MACAddress);
 		if (rc != 0) {
